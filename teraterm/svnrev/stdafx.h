821e1bdc3f306bc71811d65ebd440743692b6ee8
maya <5668936+nmaya@users.noreply.github.com>
2009-03-25 15:39:23 +0000

    SVN　リビジョンを表示するプログラムを作成し、ttpdlg のビルド前イベントで svnversion.h に落とすようにした。  これで OK なら svnversion.h を SVN 管理から外せるはず。
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@3236 f5f01b69-1e22-0410-acbf-894ab4bd6246

---
+++ teraterm/svnrev/stdafx.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN
+#endif
+
+#include <afx.h>
+#include <afxwin.h>
---
+++ teraterm/svnrev/svnrev.cpp
@@ -0,0 +1,62 @@
+#include "stdafx.h"
+#include "svnrev.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+CWinApp theApp;
+
+using namespace std;
+
+int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
+{
+	int nRetCode = 0;
+	int revision = -1;
+
+	if (!AfxWinInit(::GetModuleHandle(NULL), NULL, ::GetCommandLine(), 0))
+	{
+		nRetCode = 1;
+	}
+	else
+	{
+		BOOL ret;
+		CStdioFile csf;
+		CString cs;
+		int format = -1;
+		
+		// [root dir]\.svn\entries
+		ret = csf.Open("..\\..\\.svn\\entries", CFile::modeRead);
+		if (ret == FALSE) {
+			nRetCode = 1;
+		}
+		else {
+			csf.SeekToBegin();
+
+			// line 1
+			csf.ReadString(cs);
+			format = atoi(cs);
+
+			if (format == 8 || format == 9 || format == 9) {
+				// skip line 2 name, 3 kind
+				csf.ReadString(cs);
+				csf.ReadString(cs);
+
+				// line 4 revision
+				csf.ReadString(cs);
+				revision = atoi(cs);
+			}
+
+			csf.Close();
+		}
+	}
+
+	if (nRetCode == 0) {
+		printf("#define SVNVERSION %d", revision);
+	}
+	else {
+		printf("#undef SVNVERSION");
+	}
+
+	return nRetCode;
+}
