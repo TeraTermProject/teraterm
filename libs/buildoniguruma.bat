19052478cd863eb3c3c82cd5f3e77ab8fd1d1d6c
maya <5668936+nmaya@users.noreply.github.com>
2009-03-28 11:57:23 +0000

    ライブラリの出力先やファイル名を変更
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@3255 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- libs/buildoniguruma.bat
+++ libs/buildoniguruma.bat
@@ -4,13 +4,17 @@ if exist "Makefile" goto build
 
 copy win32\config.h config.h
 perl -e "open(IN,'win32\Makefile');while(<IN>){s|CFLAGS =|CFLAGS = /MT|;print $_;}close(IN);" > Makefile
-perl -e "open(IN,'win32\Makefile');while(<IN>){s|CFLAGS =|CFLAGS = /MTd|;s|libname   = |libname   = debug/|;print $_;}close(IN);" > Makefile.debug
-mkdir debug
+perl -e "open(IN,'win32\Makefile');while(<IN>){s|CFLAGS = -O2|CFLAGS = /MTd -Od|;s|_s.lib|_sd.lib|;print $_;}close(IN);" > Makefile.debug
 
 :build
-nmake clean
+if exist onig_sd.lib goto build_release
+nmake -f Makefile.debug clean
 nmake -f Makefile.debug
+move onig_sd.lib sample\
 nmake clean
+move sample\onig_sd.lib .\
+
+:build_release
 nmake
 
 cd ..
--- libs/buildzlib.bat
+++ libs/buildzlib.bat
@@ -1,22 +1,20 @@
 cd zlib
 
-if exist "win32\Makefile.msc.rel" goto build
+if exist "win32\Makefile.msc.release" goto build
 
-mkdir debug
-mkdir release
+perl -e "open(IN,'win32\Makefile.msc');while(<IN>){s/ -MD/ -MT/;print $_;}close(IN);" > win32\Makefile.msc.release
 
-perl -e "open(IN,'win32\Makefile.msc');while(<IN>){s/ -MD/ -MT/;print $_;}close(IN);" > win32\Makefile.tmp
-copy /Y win32\Makefile.tmp win32\Makefile.msc.rel
-del win32\Makefile.tmp
-
-perl -e "open(IN,'win32\Makefile.msc');while(<IN>){s/ -MD -O2/ -MTd -Od/;s/ -release/ -debug/;s/ zlib.lib/ zlibd.lib/;s/ zlib.lib/ zlibd.lib/;print $_;}close(IN);" > win32\Makefile.tmp
-copy /Y win32\Makefile.tmp win32\Makefile.msc.deb
-del win32\Makefile.tmp
+perl -e "open(IN,'win32\Makefile.msc');while(<IN>){s/ -MD -O2/ -MTd -Od/;s/ -release/ -debug/;s/ zlib.lib/ zlibd.lib/;s/ zlib.lib/ zlibd.lib/;print $_;}close(IN);" > win32\Makefile.msc.debug
 
 :build
-nmake -f win32\Makefile.msc.rel clean all
-copy /Y zlib.lib release\
-nmake -f win32\Makefile.msc.deb clean all
-copy /Y zlibd.lib debug\
+if exist zlibd.lib goto build_release
+nmake -f win32\Makefile.msc.debug clean
+nmake -f win32\Makefile.msc.debug
+move zlibd.lib examples\
+nmake -f win32\Makefile.msc.release clean
+move examples\zlibd.lib .\
+
+:build_release
+nmake -f win32\Makefile.msc.release
 
 cd ..
