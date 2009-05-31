37774301de3e0128cfa631ca44922c69160191b6
yutakapon <yutakapon>
2009-05-31 13:59:54 +0000

    ActivePerl で動作するように改修した。
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@3430 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- installer/check_sjis_code.pl
+++ installer/check_sjis_code.pl
@@ -3,15 +3,41 @@
 #
 # 英語版ドキュメントに日本語が含まれていないかを調べる。
 #
-# Usage(on Cygwin):
-#  perl ./check_sjis_code.pl ../doc/en/*/*/*.html > result.txt
+# Usage(ActivePerl):
+#  perl check_sjis_code.pl > result.txt
 #
 
-for ($i = 0 ; $i < @ARGV ; $i++) {
-#	print "$ARGV[$i]\n";
-	check_sjis_code($ARGV[$i]);
-}
+get_file_paths('../doc/en/html');
 exit(0);
+
+sub get_file_paths {
+	my ($top_dir)= @_;
+	my @paths=();
+	my @temp = ();
+
+	#-- カレントの一覧を取得 --#
+	opendir(DIR, $top_dir);
+	@temp = readdir(DIR);
+	closedir(DIR);
+	foreach my $path (sort @temp) {
+		next if( $path =~ /^\.{1,2}$/ );                # '.' と '..' はスキップ
+		next if( $path =~ /^\.svn$/ );                # '.svn' はスキップ
+		
+		my $full_path = "$top_dir" . '/' . "$path";
+		next if (-B $full_path);     # バイナリファイルはスキップ
+		
+#		print "$full_path\r\n";                     # 表示だけなら全てを表示してくれる-------
+		push(@paths, $full_path);                       # データとして取り込んでも前の取り込みが初期化される
+		if( -d "$top_dir/$path" ){                      #-- ディレクトリの場合は自分自身を呼び出す
+			&get_file_paths("$full_path");
+			
+		} else {
+			check_sjis_code($full_path);
+		
+		}
+	}
+	return \@paths;
+}
 
 
 # cf. http://charset.7jp.net/sjis.html
