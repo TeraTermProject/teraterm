f5cf095d8a36fd553972f33908723d6f8d3466e5
maya <5668936+nmaya@users.noreply.github.com>
2009-08-27 08:38:39 +0000

    TCPPort をセットするようにした
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@3612 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- installer/setini.pl
+++ installer/setini.pl
@@ -7,6 +7,7 @@ while(<INI>){
 	s/^(CodePage\s*=).*$/${1}932/;
 	s/^(VTFont\s*=).*$/$1‚l‚r –¾’©,0,-16,128/;
 	s/^(TEKFont\s*=).*$/$1Terminal,0,8,128/;
+	s/^(TCPPort\s*=).*$/${1}22/;
 	print;
 }
 close INI;
