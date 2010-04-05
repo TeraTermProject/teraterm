a11a31982b9a74cbd5337d26332cb824e9aa7630
IWAMOTO Kouichi <sue@iwmt.org>
2010-04-05 06:34:24 +0000

    zlib 1.2.4 で Debug ビルドに失敗するのを修正。
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@3843 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- libs/buildzlib.bat
+++ libs/buildzlib.bat
@@ -11,7 +11,7 @@ nmake -f win32\Makefile.msc.release clean
 :mkmf
 perl -e "open(IN,'win32\Makefile.msc');while(<IN>){s/ -MD/ -MT/;print $_;}close(IN);" > win32\Makefile.msc.release
 
-perl -e "open(IN,'win32\Makefile.msc');while(<IN>){s/ -MD -O2/ -MTd -Od/;s/ -release/ -debug/;s/ zlib.lib/ zlibd.lib/;s/ zlib.lib/ zlibd.lib/;print $_;}close(IN);" > win32\Makefile.msc.debug
+perl -e "open(IN,'win32\Makefile.msc');while(<IN>){s/ -MD/ -MTd/;s/ -O2/ -Od/;s/ -release/ -debug/;s/ zlib.lib/ zlibd.lib/;s/ zlib.lib/ zlibd.lib/;print $_;}close(IN);" > win32\Makefile.msc.debug
 
 :build
 if exist zlibd.lib goto build_release
