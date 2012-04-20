6bedd49e99afca1c45f6201247105192ef1418c5
yutakapon <yutakapon>
2012-04-20 14:05:01 +0000

    環境変数を使うように修正。 しょせん、オレオレ証明書なので、「不明な発行元」というセキュリティ警告は出されるが、 EXEファイルにメールアドレス等を含めることができる。 ＃RLoginにはオレオレが付いていた。
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@4908 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- installer/codesigning.bat
+++ installer/codesigning.bat
@@ -7,26 +7,32 @@ rem
 if "%1"=="/?" goto help
 if "%1"=="" goto help
 
-SET exe="%1"
-SET openssl=..\libs\openssl\out32\openssl.exe
-SET sslconf=..\libs\openssl\apps\openssl.cnf
-SET pass="teraterm"
+SET EXEFILE="%1"
+SET OPENSSL=..\libs\openssl\out32\openssl.exe
+SET SSLCONF=..\libs\openssl\apps\openssl.cnf
+SET PASSWD="teraterm"
+SET DAYS=365
+SET PRIVKEY=cakey.pem
+SET CERTKEY=cacert.pem
+SET PFXKEY=certificate.pfx
+SET TIMESERV=http://www.trustcenter.de/codesigning/timestamp
+rem SET TIMESERV=http://timestamp.verisign.com/scripts/timstamp.dll
 
 rem SSL証明書と秘密鍵を作成する。
 
-del /q cakey.pem cacert.pem certificate.pfx
-echo パスフレーズは %pass% を入れてください
+del /q %PRIVKEY% %CERTKEY% %PFXKEY%
+echo パスフレーズは %PASSWD% を入れてください
 
-%openssl% req -new -x509 -keyout cakey.pem -out cacert.pem -days 365 -config %sslconf%
-%openssl% pkcs12 -export -out certificate.pfx -inkey cakey.pem -in cacert.pem
-signtool sign /f certificate.pfx /a /t http://timestamp.verisign.com/scripts/timstamp.dll /p teraterm %exe%
+%openssl% req -new -x509 -keyout %PRIVKEY% -out %CERTKEY% -days %DAYS% -config %SSLCONF%
+%openssl% pkcs12 -export -out %PFXKEY% -inkey %PRIVKEY% -in %CERTKEY%
+signtool sign /f %PFXKEY% /a /t %TIMESERV% /p %PASSWD% %EXEFILE%
 
 exit /b
 
 :help
 echo OpenSSL + signtool を使って、オレオレコードサイニング証明書を付与する。
 echo.
+echo Usage:
 echo   %0 Output\teraterm-4.72-RC1.exe
-echo.
 exit /b
 
