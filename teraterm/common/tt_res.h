dd4cf055b5a86c5ca3e25f384b36121faef62138
yutakapon <yutakapon>
2013-05-05 12:41:52 +0000

    File メニューに Exit All を追加した。 IDが52000番台になってしまったのが、いまひとつ。
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@5233 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- teraterm/common/tt_res.h
+++ teraterm/common/tt_res.h
@@ -253,6 +253,7 @@
 #define ID_TEKHELP_ABOUT                51990
 #define ID_EDIT_EXTERNALSETUP           52005
 #define ID_FILE_TERATERMMENU            52006
+#define ID_FILE_EXITALL                 52031
 
 // Next default values for new objects
 // 
@@ -260,7 +261,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        123
-#define _APS_NEXT_COMMAND_VALUE         52031
+#define _APS_NEXT_COMMAND_VALUE         52032
 #define _APS_NEXT_CONTROL_VALUE         2534
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
