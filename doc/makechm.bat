6763547f16951918d57309b764e1ed8f88221028
yutakapon <yutakapon>
2013-08-18 10:50:37 +0000

    日本語版Windows 以外でも動作するように修正した。
    cf.
    http://logmett.com/forum/viewtopic.php?f=2&t=2338
    
    
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@5362 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- doc/makechm.bat
+++ doc/makechm.bat
@@ -5,9 +5,18 @@ if exist "%Programfiles(x86)%\HTML Help Workshop\hhc.exe" (
 	set HELP_COMPILER=C:\progra~1\htmlhe~1\hhc.exe
 )
 
+REM for winxp installation on other drive or other languaje
+if exist "%Programfiles%\HTML Help Workshop\hhc.exe" (
+set HELP_COMPILER="%Programfiles%\HTML Help Workshop\hhc.exe"
+)
+
 set updated=
 
 CALL convtext.bat
+
+REM Check Japanese version Windows
+chcp | find "932" > NUL
+if ERRORLEVEL 1 goto English
 
 for /f "delims=" %%i in ('perl htmlhelp_update_check.pl ja teratermj.chm') do @set updated=%%i
 if "%updated%"=="updated" (
@@ -15,6 +24,7 @@ perl htmlhelp_index_make.pl ja html > ja\Index.hhk
 %HELP_COMPILER% ja\teraterm.hhp
 )
 
+:English
 for /f "delims=" %%i in ('perl htmlhelp_update_check.pl en teraterm.chm') do @set updated=%%i
 if "%updated%"=="updated" (
 perl htmlhelp_index_make.pl en html > en\Index.hhk
