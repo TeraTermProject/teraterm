0b547e8c88d2e3a7fd9d590faf4b6fcff7a65a8d
IWAMOTO Kouichi <sue@iwmt.org>
2014-08-18 10:04:00 +0000

    メニュー項目として画面の高さ/幅いっぱいというのを設定できるようにした。 設定で-1を指定すると画面いっぱいとして扱う。 # 0で変更無し、-1で画面いっぱいというのは判り辛そうだが、いい案が思い浮かばず
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@5647 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- TTXSamples/TTXResizeMenu/ReadMe.txt
+++ TTXSamples/TTXResizeMenu/ReadMe.txt
@@ -2,13 +2,23 @@ TTXResizeMenu -- Resizing VT window
 
 Feature:
   This feature adds Resize menu.
-  If a user selects the Resize menu item, Tera Term VT window is changed to the specified size.
+  If a user selects the Resize menu item, Tera Term VT window is changed to
+  the specified size.
 
 Description:
   TTXResizeMenu is based on TTXResizeWin plugin. 
-  Please refer to TTXResizeWin plugin description about the detail implementation of TTXResizeMenu plugin.
+  Please refer to TTXResizeWin plugin description about the detail
+  implementation of TTXResizeMenu plugin.
 
-  If you want to change the menu items, change resize_list and recompile this plugin soure code.
+  Basically, the [Resize] menu does not display after the plugin is installed.
+  Next, a user must register the menu entry in the teraterm.ini file. 
+
+  Add a [Resize Menu] section in the teraterm.ini file, and describe the
+  "ResizeMenuN = X, Y" entry. N is an 1-origin number.
+  When X or Y equals zero, the value is not changed. For example,
+  (X, Y)=(0, 37) means that the width is not changed and the height is changed
+  into 37 lines.
+  These value maximum is 20.
 
 ToDo:
   To configure with TERATERM.INI file againt Resize menu item.
--- TTXSamples/TTXResizeMenu/TTXResizeMenu.c
+++ TTXSamples/TTXResizeMenu/TTXResizeMenu.c
@@ -21,6 +21,7 @@ typedef struct {
   PComVar cv;
   HMENU ResizeMenu;
   BOOL ReplaceTermDlg;
+  BOOL useMultiMonitorAPI;
   PReadIniFile origReadIniFile;
   int MenuItems;
   int ResizeList[MAX_MENU_ITEMS][2];
@@ -31,8 +32,76 @@ static TInstVar FAR * pvar;
 /* WIN32 allows multiple instances of a DLL */
 static TInstVar InstVar;
 
+BOOL GetMonitorSizeByChar(int *width, int *height) {
+  RECT rc_dsk, rc_wnd, rc_cl;
+
+  if (width) {
+    *width = 0;
+  }
+  if (height) {
+    *height = 0;
+  }
+
+  if (pvar->useMultiMonitorAPI) {
+    HMONITOR hm;
+    MONITORINFO mi;
+
+    hm = MonitorFromWindow(pvar->cv->HWin, MONITOR_DEFAULTTONEAREST);
+	mi.cbSize = sizeof(MONITORINFO);
+    if (! GetMonitorInfo(hm, &mi)) {
+      return FALSE;
+    }
+    rc_dsk = mi.rcWork;
+  }
+  else {
+    SystemParametersInfo(SPI_GETWORKAREA, 0, &rc_dsk, 0);
+  }
+
+  if (!GetWindowRect(pvar->cv->HWin, &rc_wnd) || !GetClientRect(pvar->cv->HWin, &rc_cl)) {
+    return FALSE;
+  }
+
+
+  if (width) {
+    int margin_w, cell_w;
+
+    margin_w = (rc_wnd.right - rc_wnd.left) - (rc_cl.right - rc_cl.left);
+    cell_w = (rc_cl.right - rc_cl.left) / pvar->ts->TerminalWidth;
+
+    *width = (rc_dsk.right - rc_dsk.left - margin_w) / cell_w;
+  }
+  if (height) {
+    int margin_h, cell_h;
+
+    margin_h = (rc_wnd.bottom - rc_wnd.top) - (rc_cl.bottom - rc_cl.top);
+    cell_h = (rc_cl.bottom - rc_cl.top) / pvar->ts->TerminalHeight;
+
+    *height = (rc_dsk.bottom - rc_dsk.top - margin_h) / cell_h;
+  }
+
+  return TRUE;
+}
+
+int mkMenuEntry(char *buff, size_t buffsize, int x, int y, int c) {
+  char tmp[20];
+
+  if (x == 0)
+    _snprintf_s(tmp, sizeof(tmp), _TRUNCATE, "height: %d", y);
+  else if (y == 0)
+    _snprintf_s(tmp, sizeof(tmp), _TRUNCATE, "width: %d", x);
+  else
+    _snprintf_s(tmp, sizeof(tmp), _TRUNCATE, "%dx%d", x, y);
+
+  if (c < 15)
+    return _snprintf_s(buff, buffsize, _TRUNCATE, "%s(&%x)", tmp, c+1);
+  else if (c < 35)
+    return _snprintf_s(buff, buffsize, _TRUNCATE, "%s(&%c)", tmp, 'a' + 1 - 9);
+  else
+    return _snprintf_s(buff, buffsize, _TRUNCATE, "%s", tmp);
+}
+
 void InitMenu() {
-  int i, x, y;
+  int i, x, y, full_w, full_h;
   char tmp[20];
 
   if (pvar->ResizeMenu != NULL) {
@@ -40,35 +109,18 @@ void InitMenu() {
   }
 
   if (pvar->MenuItems > 0) {
+    GetMonitorSizeByChar(&full_w, &full_h);
     pvar->ResizeMenu = CreateMenu();
 
     for (i=0; i < pvar->MenuItems; i++) {
       x = pvar->ResizeList[i][0];
+      if (x == -1)
+	x = full_w;
       y = pvar->ResizeList[i][1];
-      if (i < 15) {
-	if (x == 0)
-	  _snprintf_s(tmp, sizeof(tmp), _TRUNCATE, "height: %d(&%x)", y, i+1);
-	else if (y == 0)
-	  _snprintf_s(tmp, sizeof(tmp), _TRUNCATE, "width: %d(&%x)", x, i+1);
-	else
-	  _snprintf_s(tmp, sizeof(tmp), _TRUNCATE, "%dx%d(&%x)", x, y, i+1);
-      }
-      else if (i < 35) {
-	if (x == 0)
-	  _snprintf_s(tmp, sizeof(tmp), _TRUNCATE, "height: %d(&%c)", y, 'a' + i - 9);
-	else if (y == 0)
-	  _snprintf_s(tmp, sizeof(tmp), _TRUNCATE, "width: %d(&%c)", x, 'a' + i - 9);
-	else
-	  _snprintf_s(tmp, sizeof(tmp), _TRUNCATE, "%dx%d(&%c)", x, y, 'a' + i - 9);
-      }
-      else {
-	if (x == 0)
-	  _snprintf_s(tmp, sizeof(tmp), _TRUNCATE, "height: %d", y);
-	else if (y == 0)
-	  _snprintf_s(tmp, sizeof(tmp), _TRUNCATE, "width: %d", x);
-	else
-	  _snprintf_s(tmp, sizeof(tmp), _TRUNCATE, "%dx%d", x, y);
-      }
+      if (y == -1)
+	y = full_h;
+
+      mkMenuEntry(tmp, sizeof(tmp), x, y, i);
       InsertMenu(pvar->ResizeMenu, -1, MF_BYPOSITION, ID_MENUID_BASE+i, tmp);
     }
   }
@@ -77,12 +129,47 @@ void InitMenu() {
   }
 }
 
+void UpdateMenu() {
+  int i, x, y, full_w, full_h;
+  char tmp[20];
+
+  if (pvar->ResizeMenu == NULL)
+    return;
+
+  GetMonitorSizeByChar(&full_w, &full_h);
+
+  for (i=0; i < pvar->MenuItems; i++) {
+    x = pvar->ResizeList[i][0];
+    y = pvar->ResizeList[i][1];
+    if (x == -1 || y == -1) {
+      if (x == -1)
+	x = full_w;
+      if (y == -1)
+	y = full_h;
+      mkMenuEntry(tmp, sizeof(tmp), (x==-1)?full_w:x, (y==-1)?full_h:y, i);
+      ModifyMenu(pvar->ResizeMenu, i, MF_BYPOSITION, ID_MENUID_BASE+i, tmp);
+    }
+  }
+}
+
 static void PASCAL FAR TTXInit(PTTSet ts, PComVar cv) {
+  OSVERSIONINFO osvi;
+
   pvar->ts = ts;
   pvar->cv = cv;
   pvar->ReplaceTermDlg = FALSE;
   pvar->ResizeMenu = NULL;
   pvar->MenuItems = 0;
+
+  osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+  GetVersionEx(&osvi);
+  if ((osvi.dwPlatformId == VER_PLATFORM_WIN32_NT && osvi.dwMajorVersion == 4) ||
+      (osvi.dwPlatformId == VER_PLATFORM_WIN32_WINDOWS && osvi.dwMinorVersion < 10)) {
+    pvar->useMultiMonitorAPI = FALSE;
+  }
+  else {
+    pvar->useMultiMonitorAPI = TRUE;
+  }
 }
 
 static BOOL FAR PASCAL TTXSetupTerminal(HWND parent, PTTSet ts) {
@@ -108,11 +195,11 @@ static void PASCAL FAR ResizeMenuReadIniFile(PCHAR fn, PTTSet ts) {
     GetPrivateProfileString(SECTION, Key, "\n", Buff, sizeof(Buff), fn);
 
     if (sscanf_s(Buff, "%d , %d", &x, &y) == 2) {
-      if (x < 0 ) {
+      if (x < -1 ) {
         x = 0;
       }
 
-      if (y < 0 ) {
+      if (y < -1 ) {
         y = 0;
       }
 
@@ -160,18 +247,33 @@ static void PASCAL FAR TTXModifyMenu(HMENU menu) {
   }
 }
 
-static int PASCAL FAR TTXProcessCommand(HWND hWin, WORD cmd) {
-  int num;
+static void PASCAL FAR TTXModifyPopupMenu(HMENU menu) {
+  if (menu == pvar->ResizeMenu) {
+    UpdateMenu();
+  }
+}
+
+static int PASCAL FAR TTXProcessCommand(HWND HWin, WORD cmd) {
+  int num, full_h, full_w;
   if (cmd >= ID_MENUID_BASE && cmd < ID_MENUID_BASE + pvar->MenuItems) {
+    GetMonitorSizeByChar(&full_w, &full_h);
     num = cmd - ID_MENUID_BASE;
-    if (pvar->ResizeList[num][0] > 0)
+    if (pvar->ResizeList[num][0] > 0) {
       pvar->ts->TerminalWidth = pvar->ResizeList[num][0];
-    if (pvar->ResizeList[num][1] > 0)
+    }
+    else if (pvar->ResizeList[num][0] == -1 && full_w > 0) {
+      pvar->ts->TerminalWidth = full_w;
+    }
+    if (pvar->ResizeList[num][1] > 0) {
       pvar->ts->TerminalHeight = pvar->ResizeList[num][1];
+    }
+    else if (pvar->ResizeList[num][1] == -1 && full_h > 0) {
+      pvar->ts->TerminalHeight = full_h;
+    }
     pvar->ReplaceTermDlg = TRUE;
 
     // Call Setup-Terminal dialog
-    SendMessage(hWin, WM_COMMAND, MAKELONG(ID_SETUP_TERMINAL, 0), 0);
+    SendMessage(HWin, WM_COMMAND, MAKELONG(ID_SETUP_TERMINAL, 0), 0);
     return 1;
   }
   return 0;
@@ -188,7 +290,7 @@ static TTXExports Exports = {
   NULL, // TTXCloseTCP,
   NULL, // TTXSetWinSize,
   TTXModifyMenu,
-  NULL, // TTXModifyPopupMenu,
+  TTXModifyPopupMenu,
   TTXProcessCommand,
   NULL, // TTXEnd
 };
