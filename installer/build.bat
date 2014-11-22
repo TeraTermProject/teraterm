2c408cf9958a6da3aa07314ef5a382f6bd8f7a3c
yutakapon <yutakapon>
2014-11-22 16:47:50 +0000

    slnファイルの指定を変数化した。
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@5716 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- installer/build.bat
+++ installer/build.bat
@@ -1,3 +1,18 @@
+
+set TERATERMSLN=..\teraterm\ttermpro.sln
+set TTSSHSLN=..\ttssh2\ttssh.sln
+set TTPROXYSLN=..\TTProxy\TTProxy.sln
+set TTXKANJISLN=..\TTXKanjiMenu\ttxkanjimenu.sln
+set TTPMENUSLN=..\ttpmenu\ttpmenu.sln
+set TTXSAMPLESLN=..\TTXSamples\TTXSamples.sln
+
+rem set TERATERMSLN=..\teraterm\ttermpro.v12.sln
+rem set TTSSHSLN=..\ttssh2\ttssh.v12.sln
+rem set TTPROXYSLN=..\TTProxy\TTProxy.v12.sln
+rem set TTXKANJISLN=..\TTXKanjiMenu\ttxkanjimenu.v12.sln
+rem set TTPMENUSLN=..\ttpmenu\ttpmenu.v12.sln
+rem set TTXSAMPLESLN=..\TTXSamples\TTXSamples.v12.sln
+
 set BUILD=build
 if "%1" == "rebuild" (set BUILD=rebuild)
 
@@ -10,18 +25,18 @@ if "%BUILD%" == "rebuild" goto build
 
 rem "rebuild"を指定しない場合、SVNリビジョンを更新する。
 if exist ..\teraterm\release\svnrev.exe goto svnrev
-devenv /build release ..\teraterm\ttermpro.sln /project svnrev /projectconfig release
+devenv /build release %TERATERMSLN% /project svnrev /projectconfig release
 
 :svnrev
 ..\teraterm\release\svnrev.exe ..\libs\svn\bin\svnversion.exe .. ..\teraterm\ttpdlg\svnversion.h
 
 :build
-devenv /%BUILD% release ..\teraterm\ttermpro.sln
-devenv /%BUILD% release ..\ttssh2\ttssh.sln
-devenv /%BUILD% release ..\TTProxy\TTProxy.sln
-devenv /%BUILD% release ..\TTXKanjiMenu\ttxkanjimenu.sln
-devenv /%BUILD% release ..\ttpmenu\ttpmenu.sln
-devenv /%BUILD% release ..\TTXSamples\TTXSamples.sln
+devenv /%BUILD% release %TERATERMSLN%
+devenv /%BUILD% release %TTSSHSLN%
+devenv /%BUILD% release %TTPROXYSLN%
+devenv /%BUILD% release %TTXKANJISLN%
+devenv /%BUILD% release %TTPMENUSLN%
+devenv /%BUILD% release 
 
 rem cygterm をコンパイル
 pushd ..\cygterm
