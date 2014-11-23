f22885b84b422652f8ec806d80563e6fbb8f8e19
yutakapon <yutakapon>
2014-11-23 10:46:30 +0000

    VS2005とVS2013の切り替えを追加した。
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@5718 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- installer/build.bat
+++ installer/build.bat
@@ -1,4 +1,17 @@
 
+rem Visual StudioÇÃÉoÅ[ÉWÉáÉìîªï 
+echo %VSINSTALLDIR% | find "Studio 8"
+if not ERRORLEVEL 1 goto vs2005
+
+set TERATERMSLN=..\teraterm\ttermpro.v12.sln
+set TTSSHSLN=..\ttssh2\ttssh.v12.sln
+set TTPROXYSLN=..\TTProxy\TTProxy.v12.sln
+set TTXKANJISLN=..\TTXKanjiMenu\ttxkanjimenu.v12.sln
+set TTPMENUSLN=..\ttpmenu\ttpmenu.v12.sln
+set TTXSAMPLESLN=..\TTXSamples\TTXSamples.v12.sln
+goto vsend
+
+:vs2005
 set TERATERMSLN=..\teraterm\ttermpro.sln
 set TTSSHSLN=..\ttssh2\ttssh.sln
 set TTPROXYSLN=..\TTProxy\TTProxy.sln
@@ -6,12 +19,7 @@ set TTXKANJISLN=..\TTXKanjiMenu\ttxkanjimenu.sln
 set TTPMENUSLN=..\ttpmenu\ttpmenu.sln
 set TTXSAMPLESLN=..\TTXSamples\TTXSamples.sln
 
-rem set TERATERMSLN=..\teraterm\ttermpro.v12.sln
-rem set TTSSHSLN=..\ttssh2\ttssh.v12.sln
-rem set TTPROXYSLN=..\TTProxy\TTProxy.v12.sln
-rem set TTXKANJISLN=..\TTXKanjiMenu\ttxkanjimenu.v12.sln
-rem set TTPMENUSLN=..\ttpmenu\ttpmenu.v12.sln
-rem set TTXSAMPLESLN=..\TTXSamples\TTXSamples.v12.sln
+:vsend
 
 set BUILD=build
 if "%1" == "rebuild" (set BUILD=rebuild)
