e8bab71514800fed198bd610251eca2cc3e1f58c
maya <5668936+nmaya@users.noreply.github.com>
2015-02-03 10:31:02 +0000

    リファクタリング
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@5776 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- libs/buildopenssl.bat
+++ libs/buildopenssl.bat
@@ -1,12 +1,11 @@
 cd openssl
 
-if exist "Makefile.bak" goto build
+if exist "ms\ntd.mak" goto build
 
 perl Configure no-asm VC-WIN32
 
 copy ms\do_ms.bat ms\do_ms.bat.orig
-perl -e "open(IN,'ms\do_ms.bat');while(<IN>){print $_;}print 'perl util\mk1mf.pl no-asm debug VC-WIN32 >ms\ntd.mak';print \"\n\";close(IN);" > ms\do_ms.bat.tmp
-copy /Y ms\do_ms.bat.tmp ms\do_ms.bat
+echo perl util\mk1mf.pl no-asm debug VC-WIN32 ^>ms\ntd.mak>>ms\do_ms.bat
 
 call ms\do_ms.bat
 
