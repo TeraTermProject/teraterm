83779d8c7cd77dcf82ef05c21d0d23f09d4f28a7
maya <5668936+nmaya@users.noreply.github.com>
2015-11-09 14:24:48 +0000

    Visual Stuido 2005 でコンパイルできないので修正
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@6101 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- teraterm/teraterm/vtwin.cpp
+++ teraterm/teraterm/vtwin.cpp
@@ -57,7 +57,9 @@
 #include "winjump.h"
 
 #include "initguid.h"
-#include "Usbiodef.h"
+//#include "Usbiodef.h"
+DEFINE_GUID(GUID_DEVINTERFACE_USB_DEVICE, 0xA5DCBF10L, 0x6530, 0x11D2, 0x90, 0x1F, 0x00, \
+             0xC0, 0x4F, 0xB9, 0x51, 0xED);
 
 #define VTClassName "VTWin32"
 
