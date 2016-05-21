83759ffb977113c02a4df9d954131c5c513efb5a
maya <5668936+nmaya@users.noreply.github.com>
2016-05-21 00:11:00 +0000

    鬼車6用のバッチファイル(仮)   フォルダ構成が変更されている ... 参照する側で変更が必要と思われる   6.0.0 は Visual Studio 2005 でビルドできない
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@6378 f5f01b69-1e22-0410-acbf-894ab4bd6246

---
+++ libs/buildoniguruma6.bat
@@ -0,0 +1,23 @@
+cd onig-6.0.0\src
+
+if exist "Makefile" goto build
+del onig_sd.lib
+nmake clean
+copy config.h.win32 config.h
+perl -e "open(IN,'Makefile.windows');while(<IN>){s|CFLAGS =|CFLAGS = /MT|;print $_;}close(IN);" > Makefile
+perl -e "open(IN,'Makefile.windows');while(<IN>){s|CFLAGS = -O2|CFLAGS = /MTd -Od|;s|_s.lib|_sd.lib|;print $_;}close(IN);" > Makefile.debug
+
+:build
+if exist "onig_sd.lib" goto build_release
+nmake -f Makefile.debug clean
+nmake -f Makefile.debug
+move onig_sd.lib ..\sample\
+nmake clean
+move ..\sample\onig_sd.lib .\
+
+:build_release
+if exist "onig_s.lib" goto end
+nmake
+
+:end
+cd ..\..
