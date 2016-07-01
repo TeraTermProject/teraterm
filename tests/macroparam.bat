b6fb3e3060931ac79ff2c5358924233da313255e
IWAMOTO Kouichi <sue@iwmt.org>
2016-07-01 03:14:23 +0000

    テストバッチファイル/マクロを更新
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@6425 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- tests/macroparam.bat
+++ tests/macroparam.bat
@@ -4,15 +4,24 @@ set TTMACRO=%~dp0\..\teraterm\Debug\ttpmacro.exe
 set MACROFILE=%~dpn0.ttl
 
 echo == test 1 ==
-"%TTMACRO%" "%MACROFILE%" /vxx /ixx /v /i test1
+"%TTMACRO%" "%MACROFILE%" /vxx /ixx /V /i test1
 if ERRORLEVEL 1 echo test 1 failed.
 
 echo == test 2 ==
-"%TTMACRO%" "%MACROFILE%" /v /i test2
+"%TTMACRO%" /V /i "%MACROFILE%" /v /I test2
 if ERRORLEVEL 1 echo test 2 failed.
 
 echo == test 3 ==
-"%TTMACRO%" "%MACROFILE%" test3 /vxx /ixx /v /i
+"%TTMACRO%" /I "%MACROFILE%" test3 /Vxx /ixx /V /i
 if ERRORLEVEL 1 echo test 3 failed.
+
+echo == test 4 ==
+"%TTMACRO%" /i "%MACROFILE%" test4 /V /Vxx /ixx
+if ERRORLEVEL 1 echo test 4 failed.
+
+rem ; ttpmacro.exe macrofile /vxx /ixx /V /i test1
+rem ; ttpmacro.exe /V /i macrofile /v /I test2
+rem ; ttpmacro.exe /I macrofile test3 /Vxx /ixx /V /i
+rem ; ttpmacro.exe /i macrofile test4 /V /Vxx /ixx
 
 pause
