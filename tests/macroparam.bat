b5d694a9735877d1e2fbd74ce0ed098a3af03818
IWAMOTO Kouichi <sue@iwmt.org>
2016-07-07 11:45:59 +0000

    bat ファイルを書くのに参考にした記述を削除し忘れていたけれど、 残っていてもいいかなと思ったのでふさわしい場所に移動。
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@6433 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- tests/macroparam.bat
+++ tests/macroparam.bat
@@ -4,24 +4,23 @@ set TTMACRO=%~dp0\..\teraterm\Debug\ttpmacro.exe
 set MACROFILE=%~dpn0.ttl
 
 echo == test 1 ==
+rem ; ttpmacro.exe macrofile /vxx /ixx /V /i test1
 "%TTMACRO%" "%MACROFILE%" /vxx /ixx /V /i test1
 if ERRORLEVEL 1 echo test 1 failed.
 
 echo == test 2 ==
+rem ; ttpmacro.exe /V /i macrofile /v /I test2
 "%TTMACRO%" /V /i "%MACROFILE%" /v /I test2
 if ERRORLEVEL 1 echo test 2 failed.
 
 echo == test 3 ==
+rem ; ttpmacro.exe /I macrofile test3 /Vxx /ixx /V /i
 "%TTMACRO%" /I "%MACROFILE%" test3 /Vxx /ixx /V /i
 if ERRORLEVEL 1 echo test 3 failed.
 
 echo == test 4 ==
+rem ; ttpmacro.exe /i macrofile test4 /V /Vxx /ixx
 "%TTMACRO%" /i "%MACROFILE%" test4 /V /Vxx /ixx
 if ERRORLEVEL 1 echo test 4 failed.
-
-rem ; ttpmacro.exe macrofile /vxx /ixx /V /i test1
-rem ; ttpmacro.exe /V /i macrofile /v /I test2
-rem ; ttpmacro.exe /I macrofile test3 /Vxx /ixx /V /i
-rem ; ttpmacro.exe /i macrofile test4 /V /Vxx /ixx
 
 pause
