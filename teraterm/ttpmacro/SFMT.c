a02112c5190c340b819e44842ae3b395d8820a9a
IWAMOTO Kouichi <sue@iwmt.org>
2017-02-21 05:41:39 +0000

    SFMT 1.5 に更新。
    変更箇所は、
    
    ・sfmt_to_res53() の誤りの修正 (Tera Term では未使用)
    ・ARM NEON 対応? (Web Page には記述無し)
    ・SSE2 非対応でビルドした時でも SSE2 用の static 変数が確保されていた事の
      修正 (多分今までも最適化で消えてた)
    
    だけなので動作的には変わらないけど、念の為。
    
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@6599 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- teraterm/ttpmacro/SFMT.c
+++ teraterm/ttpmacro/SFMT.c
@@ -40,11 +40,6 @@ extern "C" {
 #undef ONLY64
 #endif
 
-/**
- * parameters used by sse2.
- */
-static const w128_t sse2_param_mask = {{SFMT_MSK1, SFMT_MSK2,
-                                        SFMT_MSK3, SFMT_MSK4}};
 /*----------------
   STATIC FUNCTIONS
   ----------------*/
@@ -60,11 +55,18 @@ inline static void swap(w128_t *array, int size);
 #if defined(HAVE_ALTIVEC)
   #include "SFMT-alti.h"
 #elif defined(HAVE_SSE2)
+/**
+ * parameters used by sse2.
+ */
+  static const w128_t sse2_param_mask = {{SFMT_MSK1, SFMT_MSK2,
+                                          SFMT_MSK3, SFMT_MSK4}};
   #if defined(_MSC_VER)
     #include "SFMT-sse2-msc.h"
   #else
     #include "SFMT-sse2.h"
   #endif
+#elif defined(HAVE_NEON)
+  #include "SFMT-neon.h"
 #endif
 
 /**
@@ -81,7 +83,7 @@ inline static int idxof(int i) {
 }
 #endif
 
-#if (!defined(HAVE_ALTIVEC)) && (!defined(HAVE_SSE2))
+#if (!defined(HAVE_ALTIVEC)) && (!defined(HAVE_SSE2)) && (!defined(HAVE_NEON))
 /**
  * This function fills the user-specified array with pseudorandom
  * integers.
@@ -232,7 +234,7 @@ int sfmt_get_min_array_size64(sfmt_t * sfmt) {
     return SFMT_N64;
 }
 
-#if !defined(HAVE_SSE2) && !defined(HAVE_ALTIVEC)
+#if !defined(HAVE_SSE2) && !defined(HAVE_ALTIVEC) && !defined(HAVE_NEON)
 /**
  * This function fills the internal state array with pseudorandom
  * integers.
