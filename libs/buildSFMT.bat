1b479c1ca9023362f33012eca7c9ed2bba481c8c
maya <5668936+nmaya@users.noreply.github.com>
2017-03-15 10:30:44 +0000

    SFMT のビルド用バッチファイルを追加
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@6631 f5f01b69-1e22-0410-acbf-894ab4bd6246

---
+++ libs/buildSFMT.bat
@@ -0,0 +1,28 @@
+cd SFMT
+
+if not exist "Makefile.msc.release" goto end_mk_release
+echo CFLAGS = /MT /O2 /nologo> Makefile.msc.release
+echo;>> Makefile.msc.release
+echo SFMT.lib: SFMT.c>> Makefile.msc.release
+echo 	cl $(CFLAGS) /c SFMT.c>> Makefile.msc.release
+echo 	lib /out:SFMT.lib SFMT.obj>> Makefile.msc.release
+echo;>> Makefile.msc.release
+echo clean:>> Makefile.msc.release
+echo 	del *.lib *.obj>> Makefile.msc.release
+:end_mk_release
+
+if not exist "Makefile.msc.release" goto end_mk_debug
+echo CFLAGS = /MTd /Od /nologo> Makefile.msc.debug
+echo;>> Makefile.msc.debug
+echo SFMTd.lib: SFMT.c>> Makefile.msc.debug
+echo 	cl $(CFLAGS) /c SFMT.c>> Makefile.msc.debug
+echo 	lib /out:SFMTd.lib SFMT.obj>> Makefile.msc.debug
+echo;>> Makefile.msc.debug
+echo clean:>> Makefile.msc.debug
+echo 	del *.lib *.obj>> Makefile.msc.debug
+:end_mk_debug
+
+nmake /f Makefile.msc.debug
+nmake /f Makefile.msc.release
+
+cd ..
--- libs/buildall.bat
+++ libs/buildall.bat
@@ -1,3 +1,4 @@
 CALL buildoniguruma6.bat
 CALL buildzlib.bat
 CALL buildopenssl.bat
+CALL buildSFMT.bat
