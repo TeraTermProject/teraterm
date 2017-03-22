3b8c47e6fb32041ffd989c776b25510db1b5ccd1
IWAMOTO Kouichi <sue@iwmt.org>
2017-03-22 09:19:48 +0000

    ・一行の長さが80桁に収まるように、一部を変数化 ・ショートオプションの指定を一般的な - 一個に変更 ・必要ない一部のオプション指定を省略
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@6645 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- doc/convtext.bat
+++ doc/convtext.bat
@@ -1,12 +1,15 @@
-perl ..\installer\2sjis.pl --i ..\libs\oniguruma\COPYING --o en\html\reference\Oniguruma-LICENSE.txt --c euc-jp --l unix
-perl ..\installer\2sjis.pl --i ..\libs\oniguruma\COPYING --o ja\html\reference\Oniguruma-LICENSE.txt --c euc-jp --l unix
-perl ..\installer\2sjis.pl --i ..\libs\oniguruma\doc\RE    --o en\html\reference\RE.txt --c euc-jp --l unix
-perl ..\installer\2sjis.pl --i ..\libs\oniguruma\doc\RE.ja --o ja\html\reference\RE.txt --c euc-jp --l unix
-perl ..\installer\2sjis.pl --i ..\libs\openssl\LICENSE  --o en\html\reference\OpenSSL-LICENSE.txt --c euc-jp --l unix
-perl ..\installer\2sjis.pl --i ..\libs\openssl\LICENSE  --o ja\html\reference\OpenSSL-LICENSE.txt --c euc-jp --l unix
-perl ..\installer\2sjis.pl --i ..\libs\putty\LICENCE  --o en\html\reference\PuTTY-LICENSE.txt --c shiftjis --l crlf
-perl ..\installer\2sjis.pl --i ..\libs\putty\LICENCE  --o ja\html\reference\PuTTY-LICENSE.txt --c shiftjis --l crlf
-perl ..\installer\2sjis.pl --i ..\libs\SFMT\LICENSE.txt --o en\html\reference\SFMT-LICENSE.txt --c shiftjis --l unix
-perl ..\installer\2sjis.pl --i ..\libs\SFMT\LICENSE.txt --o ja\html\reference\SFMT-LICENSE.txt --c shiftjis --l unix
-perl ..\installer\2sjis.pl --i ..\cygterm\COPYING  --o en\html\reference\CygTerm+-LICENSE.txt --c shiftjis --l unix
-perl ..\installer\2sjis.pl --i ..\cygterm\COPYING  --o ja\html\reference\CygTerm+-LICENSE.txt --c shiftjis --l unix
+set TOSJIS=perl ..\installer\2sjis.pl
+set REF_E=en\html\reference
+set REF_J=ja\html\reference
+%TOSJIS% -i ..\libs\oniguruma\COPYING   -o %REF_E%\Oniguruma-LICENSE.txt -l unix
+%TOSJIS% -i ..\libs\oniguruma\COPYING   -o %REF_J%\Oniguruma-LICENSE.txt -l unix
+%TOSJIS% -i ..\libs\oniguruma\doc\RE    -o %REF_E%\RE.txt      -c euc-jp -l unix
+%TOSJIS% -i ..\libs\oniguruma\doc\RE.ja -o %REF_J%\RE.txt      -c euc-jp -l unix
+%TOSJIS% -i ..\libs\openssl\LICENSE     -o %REF_E%\OpenSSL-LICENSE.txt   -l unix
+%TOSJIS% -i ..\libs\openssl\LICENSE     -o %REF_J%\OpenSSL-LICENSE.txt   -l unix
+%TOSJIS% -i ..\libs\putty\LICENCE       -o %REF_E%\PuTTY-LICENSE.txt     -l crlf
+%TOSJIS% -i ..\libs\putty\LICENCE       -o %REF_J%\PuTTY-LICENSE.txt     -l crlf
+%TOSJIS% -i ..\libs\SFMT\LICENSE.txt    -o %REF_E%\SFMT-LICENSE.txt      -l unix
+%TOSJIS% -i ..\libs\SFMT\LICENSE.txt    -o %REF_J%\SFMT-LICENSE.txt      -l unix
+%TOSJIS% -i ..\cygterm\COPYING          -o %REF_E%\CygTerm+-LICENSE.txt  -l unix
+%TOSJIS% -i ..\cygterm\COPYING          -o %REF_J%\CygTerm+-LICENSE.txt  -l unix
