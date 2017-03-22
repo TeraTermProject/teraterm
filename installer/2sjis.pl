44fb855161dc037bd566a8921f2c01da3583e950
IWAMOTO Kouichi <sue@iwmt.org>
2017-03-22 09:19:44 +0000

    ・オプションの扱いを厳密にした ・coding, lf を指定しなかった場合のデフォルトを設定 ・ヘルプメッセージを出力するようにした
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@6644 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- installer/2sjis.pl
+++ installer/2sjis.pl
@@ -4,13 +4,22 @@ require 5.8.0;
 use strict;
 use Encode;
 use utf8;
-use Getopt::Long;
+use Getopt::Long qw(:config posix_default no_ignore_case gnu_compat);
 
 my($in, $out, $coding, $lf, $result);
-$result = GetOptions('in=s'     => \$in,
-                     'out=s'    => \$out,
-                     'coding=s' => \$coding,
-                     'lf=s'     => \$lf);
+
+# default setting
+$coding = "shiftjis";
+$lf = "crlf";
+
+$result = GetOptions('in|i=s'     => \$in,
+                     'out|o=s'    => \$out,
+                     'coding|c=s' => \$coding,
+                     'lf|l=s'     => \$lf);
+
+if (!(defined($in) && defined($out))) {
+	die "Usage: $0 --in file --out file [ --coding input_encoding ] [ --lf line_format ]\n";
+}
 
 open (IN,  "<:$lf:encoding($coding)",   $in);
 open (OUT, '>:crlf:encoding(shiftjis)', $out);
