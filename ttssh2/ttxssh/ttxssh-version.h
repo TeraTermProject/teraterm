2df207b73e9ce4bfa0e915c0a4af884ea590beaa
IWAMOTO Kouichi <sue@iwmt.org>
2017-07-04 15:01:34 +0000

    バージョン情報をヘッダファイルに移し、そこで管理するよう変更 [Ttssh2-devel 3316]
    ttxssh-version.rc をリソースエディタで編集すると値を展開されてしまうため、通常はテキストエディタ等で変更すること。
    
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@6833 f5f01b69-1e22-0410-acbf-894ab4bd6246

---
+++ ttssh2/ttxssh/ttxssh-version.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2017 TeraTerm Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#define TTSSH_VERSION_MAJOR             2
+#define TTSSH_VERSION_MINOR             81
+#define TTSSH_TOSTR(x)                  TTSSH_TOSTR2(x)
+#define TTSSH_TOSTR2(x)                 #x
+#define TTSSH_VERSION_STR(sep)          TTSSH_TOSTR(TTSSH_VERSION_MAJOR) sep TTSSH_TOSTR(TTSSH_VERSION_MINOR)
+#define TTSSH_RES_VERSION_STR           TTSSH_VERSION_STR(", ") ", 0, 0"
