f645957e67e09af8f7668f964c512c91f2998b92
maya <5668936+nmaya@users.noreply.github.com>
2017-08-30 03:03:36 +0000

    RE.ja が UTF-8 になったのに対応
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@6928 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- doc/convtext.bat
+++ doc/convtext.bat
@@ -4,8 +4,8 @@ set REF_E=en\html\reference
 set REF_J=ja\html\reference
 %TOSJIS% -i ..\libs\oniguruma\COPYING   -o %REF_E%\Oniguruma-LICENSE.txt -l unix
 %TOSJIS% -i ..\libs\oniguruma\COPYING   -o %REF_J%\Oniguruma-LICENSE.txt -l unix
-%TOSJIS% -i ..\libs\oniguruma\doc\RE    -o %REF_E%\RE.txt      -c euc-jp -l unix
-%TOSJIS% -i ..\libs\oniguruma\doc\RE.ja -o %REF_J%\RE.txt      -c euc-jp -l unix
+%TOSJIS% -i ..\libs\oniguruma\doc\RE    -o %REF_E%\RE.txt                -l unix
+%TOSJIS% -i ..\libs\oniguruma\doc\RE.ja -o %REF_J%\RE.txt      -c utf8   -l unix
 %TOSJIS% -i ..\libs\openssl\LICENSE     -o %REF_E%\OpenSSL-LICENSE.txt   -l unix
 %TOSJIS% -i ..\libs\openssl\LICENSE     -o %REF_J%\OpenSSL-LICENSE.txt   -l unix
 %TOSJIS% -i ..\libs\putty\LICENCE       -o %REF_E%\PuTTY-LICENSE.txt     -l crlf
