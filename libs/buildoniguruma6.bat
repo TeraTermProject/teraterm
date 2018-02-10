2a0e51d5bbe321a84332fc6cde68287d75858852
maya <5668936+nmaya@users.noreply.github.com>
2018-02-10 07:25:13 +0000

    ビルド用バッチを Oniguruma 6.7.1 に対応
    ソースディレクトリ・ビルド用ディレクトリを変数で指定するようになった
    出力ディレクトリはカレントディレクトリになる
    https://github.com/kkos/oniguruma/pull/75
    
    release と debug でディレクトリを分け、ファイル名を変えなくてもよく
    できるはずだが
    - vcproj を全部変更する必要がある
    - openssl,SFMT,zlib でも同じようにする方法がわからない
    ので保留
    
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@7034 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- libs/buildoniguruma6.bat
+++ libs/buildoniguruma6.bat
@@ -1,23 +1,32 @@
-cd oniguruma\src
+pushd oniguruma\src
 
-if exist "Makefile" goto build
-del onig_sd.lib
-nmake clean
-copy config.h.win32 config.h
-perl -e "open(IN,'Makefile.windows');while(<IN>){s|CFLAGS =|CFLAGS = /MT|;print $_;}close(IN);" > Makefile
-perl -e "open(IN,'Makefile.windows');while(<IN>){s|CFLAGS = -O2|CFLAGS = /MTd -Od|;s|_s.lib|_sd.lib|;print $_;}close(IN);" > Makefile.debug
+SET ONIG_DIR=.
+SET BUILD_DIR=.
+
+if not exist "%ONIG_DIR%\Makefile" goto mkmf
+for %%F in (Makefile) do set mftime=%%~tF
+for %%F in (..\..\buildoniguruma6.bat) do set battime=%%~tF
+if "%battime%" leq "%mftime%" goto build
+
+del %ONIG_DIR%\onig_sd.lib
+nmake -f %ONIG_DIR%\Makefile clean
+
+:mkmf
+copy %ONIG_DIR%\config.h.win32 %ONIG_DIR%\config.h
+perl -e "open(IN,'%ONIG_DIR%\Makefile.windows');while(<IN>){s|CFLAGS =|CFLAGS = /MT|;print $_;}close(IN);" > %ONIG_DIR%\Makefile
+perl -e "open(IN,'%ONIG_DIR%\Makefile.windows');while(<IN>){s|CFLAGS = -O2|CFLAGS = /MTd -Od|;s|_s.lib|_sd.lib|;print $_;}close(IN);" > %ONIG_DIR%\Makefile.debug
 
 :build
-if exist "onig_sd.lib" goto build_release
-nmake -f Makefile.debug clean
-nmake -f Makefile.debug
-move onig_sd.lib ..\sample\
-nmake clean
-move ..\sample\onig_sd.lib .\
+if exist "%ONIG_DIR%\onig_sd.lib" goto build_release
+nmake -f %ONIG_DIR%\Makefile.debug clean
+nmake -f %ONIG_DIR%\Makefile.debug
+move %ONIG_DIR%\onig_sd.lib %ONIG_DIR%\..\sample\
+nmake -f %ONIG_DIR%\Makefile.debug clean
+move %ONIG_DIR%\..\sample\onig_sd.lib %ONIG_DIR%
 
 :build_release
-if exist "onig_s.lib" goto end
-nmake
+if exist "%ONIG_DIR%\onig_s.lib" goto end
+nmake -f %ONIG_DIR%\Makefile
 
 :end
-cd ..\..
+popd
