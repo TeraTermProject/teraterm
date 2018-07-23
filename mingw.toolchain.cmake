13020b1c94fd7d4f4e9334caf3cb810be078cd29
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2018-07-23 14:33:47 +0000

    CMakeLists.txt追加
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/branches/cmake@7163 f5f01b69-1e22-0410-acbf-894ab4bd6246

---
+++ mingw.toolchain.cmake
@@ -0,0 +1,29 @@
+﻿# how to build:
+# mkdir build; cd build
+# cmake .. -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=../mingw.toolchain.cmake
+# cmake --build .
+
+set(CMAKE_SYSTEM_NAME Windows)
+
+if(0)
+  set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
+  if(1)
+	set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+	set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+  endif()
+  if(0)
+	set(CMAKE_C_COMPILER x86_64-w64-mingw32-clang)
+	set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-clang++)
+  endif()
+  set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+else()
+  set(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32)
+  set(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
+  set(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
+  set(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
+endif()
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wextra")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra")
+
+set(CMAKE_TOOLCHAIN_FILE ${CMAKE_TOOLCHAIN_FILE} CACHE PATH "toolchain file")
