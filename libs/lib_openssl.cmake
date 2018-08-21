b7627e3a87408e427959cdb7d173fb6dc2f0a6e3
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2018-08-21 14:53:26 +0000

    マージミス?の調整とcmakeの調整
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/branches/cmake@7185 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- libs/lib_openssl.cmake
+++ libs/lib_openssl.cmake
@@ -1,14 +1,25 @@
 ﻿
 include(${CMAKE_CURRENT_LIST_DIR}/script_support.cmake)
 
-set(OPENSSL_INCLUDE_DIRS
-  "$<$<CONFIG:Debug>:${CMAKE_CURRENT_LIST_DIR}/openssl_${TOOLSET}_debug/include>"
-  "$<$<CONFIG:Release>:${CMAKE_CURRENT_LIST_DIR}/openssl_${TOOLSET}/include>"
+if(MINGW)
+  set(OPENSSL_INCLUDE_DIRS
+	${CMAKE_CURRENT_LIST_DIR}/openssl_${TOOLSET}/include
+	)
+
+  set(OPENSSL_LIB
+	${CMAKE_CURRENT_LIST_DIR}/openssl_${TOOLSET}/lib/libeay32.lib
   )
 
-set(OPENSSL_LIB
-  debug ${CMAKE_CURRENT_LIST_DIR}/openssl_${TOOLSET}_debug/lib/libeay32.lib
-#  debug ${CMAKE_CURRENT_LIST_DIR}/openssl_${TOOLSET}_debug/lib/ssleay32.lib
-  optimized ${CMAKE_CURRENT_LIST_DIR}/openssl_${TOOLSET}/lib/libeay32.lib
-#  optimized ${CMAKE_CURRENT_LIST_DIR}/openssl_${TOOLSET}/lib/ssleay32.lib
-  )
+else()
+
+  set(OPENSSL_INCLUDE_DIRS
+	"$<$<CONFIG:Debug>:${CMAKE_CURRENT_LIST_DIR}/openssl_${TOOLSET}_debug/include>"
+	"$<$<CONFIG:Release>:${CMAKE_CURRENT_LIST_DIR}/openssl_${TOOLSET}/include>"
+	)
+
+  set(OPENSSL_LIB
+	debug ${CMAKE_CURRENT_LIST_DIR}/openssl_${TOOLSET}_debug/lib/libeay32.lib
+	optimized ${CMAKE_CURRENT_LIST_DIR}/openssl_${TOOLSET}/lib/libeay32.lib
+	)
+
+endif()
