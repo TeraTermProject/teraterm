4b6e760b74d0da52e94713ba40a56ba44fdf4fce
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2018-09-03 10:51:56 +0000

    stdafx.cpp,hを削除
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/branches/cmake@7223 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- teraterm/teraterm/vtwin.cpp
+++ teraterm/teraterm/vtwin.cpp
@@ -88,7 +88,6 @@
 DEFINE_GUID(GUID_DEVINTERFACE_USB_DEVICE, 0xA5DCBF10L, 0x6530, 0x11D2, 0x90, 0x1F, 0x00, \
              0xC0, 0x4F, 0xB9, 0x51, 0xED);
 
-#define CWnd	TTCWnd
 #define CFrameWnd	TTCFrameWnd
 
 #define VTClassName "VTWin32"
