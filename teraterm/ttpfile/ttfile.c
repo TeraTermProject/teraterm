94d8b58eee15503c8f7a151cfbc0053929e78516
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2018-12-02 15:49:41 +0000

    Merge branch 'sharedmemory' into cmake
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/branches/cmake@7303 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- teraterm/ttpfile/ttfile.c
+++ teraterm/ttpfile/ttfile.c
@@ -1340,9 +1340,6 @@ BOOL WINAPI DllMain(HANDLE hInstance,
                     ULONG ul_reason_for_call,
                     LPVOID lpReserved)
 {
-//	PMap pm;
-//	HANDLE HMap = NULL;
-
 	hInst = hInstance;
 	switch( ul_reason_for_call ) {
 	case DLL_THREAD_ATTACH:
@@ -1353,15 +1350,6 @@ BOOL WINAPI DllMain(HANDLE hInstance,
 		break;
 	case DLL_PROCESS_ATTACH:
 		/* do process initialization */
-//		HMap = CreateFileMapping((HANDLE) 0xFFFFFFFF, NULL, PAGE_READONLY,
-//		                         0, sizeof(TMap), TT_FILEMAPNAME);
-//		if (HMap != NULL) {
-//			pm = (PMap)MapViewOfFile(HMap,FILE_MAP_READ,0,0,0);
-//			if (pm != NULL) {
-//				strncpy_s(UILanguageFile, sizeof(UILanguageFile), pm->ts.UILanguageFile, _TRUNCATE);
-//				strncpy_s(FileSendFilter, sizeof(FileSendFilter), pm->ts.FileSendFilter, _TRUNCATE);
-//			}
-//		}
 		DoCover_IsDebuggerPresent();
 		break;
 	case DLL_PROCESS_DETACH:
