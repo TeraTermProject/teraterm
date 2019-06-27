cd8244524c2cc9d4455f1b681d108501f0fc521b
yutakapon <yutakapon>
2019-06-27 10:08:23 +0000

    OpenSSLの生成をダイナミックリンクライブラリに変更した。 チケット #36876
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/branches/openssl_1_1_1_v2@7814 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- libs/buildopenssl11.bat
+++ libs/buildopenssl11.bat
@@ -5,7 +5,7 @@ if exist "out32.dbg\libcrypto.lib" goto build_dbg_end
 perl -e "open(IN,'Configurations/10-main.conf');while(<IN>){s|/WX|/W1|;print $_;}close(IN);" > conf.tmp
 move conf.tmp Configurations/10-main.conf
 
-perl Configure no-asm no-async no-shared no-capieng no-dso no-engine VC-WIN32 -D_WIN32_WINNT=0x0501 --debug
+perl Configure no-asm no-async shared no-capieng no-dso no-engine VC-WIN32 -D_WIN32_WINNT=0x0501 --debug
 perl -e "open(IN,'makefile');while(<IN>){s| /MDd| /MTd|;print $_;}close(IN);" > makefile.tmp
 if exist "makefile.dbg" del makefile.dbg
 ren makefile.tmp makefile.dbg
@@ -17,7 +17,7 @@ move libssl.lib out32.dbg
 :build_dbg_end
 
 if exist "out32\libcrypto.lib" goto build_end
-perl Configure no-asm no-async no-shared no-capieng no-dso no-engine VC-WIN32 -D_WIN32_WINNT=0x0501
+perl Configure no-asm no-async shared no-capieng no-dso no-engine VC-WIN32 -D_WIN32_WINNT=0x0501
 perl -e "open(IN,'makefile');while(<IN>){s| /MD| /MT|;print $_;}close(IN);" > makefile.tmp
 if exist "makefile" del makefile
 ren makefile.tmp makefile
