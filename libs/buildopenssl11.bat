2ed282c86a663b5965b51c144c61fc446c50cd00
yutakapon <yutakapon>
2019-06-27 11:51:43 +0000

    OpenSSLのビルド生成物をすべてフォルダに移動するようにした。 チケット #36876
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/branches/openssl_1_1_1_v2@7815 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- libs/buildopenssl11.bat
+++ libs/buildopenssl11.bat
@@ -12,8 +12,8 @@ ren makefile.tmp makefile.dbg
 nmake -f makefile.dbg clean
 nmake -f makefile.dbg
 mkdir out32.dbg
-move libcrypto.lib out32.dbg
-move libssl.lib out32.dbg
+move libcrypto* out32.dbg
+move libssl* out32.dbg
 :build_dbg_end
 
 if exist "out32\libcrypto.lib" goto build_end
@@ -24,8 +24,8 @@ ren makefile.tmp makefile
 nmake clean
 nmake
 mkdir out32
-move libcrypto.lib out32
-move libssl.lib out32
+move libcrypto* out32
+move libssl* out32
 :build_end
 
 cd ..
