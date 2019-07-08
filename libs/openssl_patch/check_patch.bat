860404da76b3eb7bd315a7b6a8c32a44e549188a
yutakapon <yutakapon>
2019-07-08 01:56:43 +0000

    OpenSSLへのパッチ適用を自動化した。 チケット #36876
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/branches/openssl_1_1_1_v2@7833 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- libs/openssl_patch/check_patch.bat
+++ libs/openssl_patch/check_patch.bat
@@ -1,20 +1,50 @@
 @echo off
 
+set folder=openssl_patch
+set cmdopt2=--binary --backup -p0
+set cmdopt1=--dry-run %cmdopt2%
+
 rem
-rem OpenSSL 1.1.1にパッチが適用されているかを確認する
+echo OpenSSL 1.1.1にパッチが適用されているかを確認します...
+echo.
 rem
 
+rem パッチコマンドの存在チェック
+set patchcmd="patch.exe"
+if exist %patchcmd% (goto cmd_true) else goto cmd_false
+
+:cmd_true
+
+
+rem パッチの適用有無をチェック
+:patch1
 findstr /c:"# undef AI_PASSIVE" ..\openssl\crypto\bio\bio_lcl.h
-if ERRORLEVEL 1 goto fail
+if ERRORLEVEL 1 goto fail1
+goto patch2
+:fail1
+pushd ..
+%folder%\patch %cmdopt1% < %folder%\ws2_32_dll_patch.txt
+%folder%\patch %cmdopt2% < %folder%\ws2_32_dll_patch.txt
+popd
 
+:patch2
 findstr /c:"running on Windows95" ..\openssl\crypto\threads_win.c
-if ERRORLEVEL 1 goto fail
+if ERRORLEVEL 1 goto fail2
+goto patch3
+:fail2
+pushd ..
+%folder%\patch %cmdopt1% < %folder%\InitializeCriticalSectionAndSpinCount_patch.txt
+%folder%\patch %cmdopt2% < %folder%\InitializeCriticalSectionAndSpinCount_patch.txt
+popd
 
+
+
+:patch3
 echo "パッチは適用されています"
 timeout 5
 goto end
 
-:fail
+:patchfail
 echo "パッチが適用されていないようです"
 set /P ANS="続行しますか？(y/n)"
 if "%ANS%"=="y" (
@@ -25,6 +55,15 @@ if "%ANS%"=="y" (
 ) else (
   goto fail
 )
+
+goto end
+
+:cmd_false
+echo パッチコマンド %patchcmd% が見つかりません
+echo 下記サイトからダウンロードしてください
+echo http://geoffair.net/projects/patch.htm
+echo.
+exit /b
 
 :end
 @echo on
