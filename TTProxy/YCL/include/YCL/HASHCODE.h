d961e5fb0254202f2172a836a84a113baec09981
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2019-07-11 15:41:19 +0000

    TTProxy,Hashtable内で正しくハッシュ値を計算できていなかったため修正
    - r7536でエンバグ
    - gcc で FARPROC が void * とマッチしないので HASHCODE.h にコンストラクタを追加
    - ticket #39376
    
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@7845 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- TTProxy/YCL/include/YCL/HASHCODE.h
+++ TTProxy/YCL/include/YCL/HASHCODE.h
@@ -27,6 +27,8 @@ public:
 	}
 	HASHCODE(const void* value):value((int)(uintptr_t) value) {
 	}
+	HASHCODE(const FARPROC value):value((int)(uintptr_t) value) {
+	}
 	HASHCODE(int value):value(value) {
 	}
 	operator int()const {
