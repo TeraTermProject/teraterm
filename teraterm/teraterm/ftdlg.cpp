34d836aa1562ca294dc776e23c2bf4abaf89c615
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2019-08-08 15:17:15 +0000

    未使用変数、未使用defineを削除
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@7926 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- teraterm/teraterm/ftdlg.cpp
+++ teraterm/teraterm/ftdlg.cpp
@@ -44,12 +44,6 @@
 #include "ftdlg.h"
 #include "teraterml.h"
 
-#ifdef _DEBUG
-#define new DEBUG_NEW
-#undef THIS_FILE
-static char THIS_FILE[] = __FILE__;
-#endif
-
 /////////////////////////////////////////////////////////////////////////////
 // CFileTransDlg dialog
 
