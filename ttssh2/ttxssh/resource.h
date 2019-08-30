f59e8326277ae165335509ba7869cad0793c236d
maya <5668936+nmaya@users.noreply.github.com>
2019-08-30 15:57:11 +0000

    r7632 で追加されたグループボックスの i18n 対応
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@8046 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- ttssh2/ttxssh/resource.h
+++ ttssh2/ttxssh/resource.h
@@ -233,6 +233,9 @@
 #define IDC_SSH_DEFAULTUSERNAME         1238
 #define IDC_SSH_WINDOWS_USERNAME        1239
 #define IDC_SSH_WINDOWS_USERNAME_TEXT   1241
+#define IDC_SSHAUTHMETHOD               1242
+#define IDC_SSH_AUTHMETHOD              1243
+#define IDC_SSH_USERNAME                1244
 
 // Next default values for new objects
 // 
@@ -240,7 +243,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        118
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1242
+#define _APS_NEXT_CONTROL_VALUE         1245
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
