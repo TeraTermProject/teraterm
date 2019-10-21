889e968c3194fa24fcd63dd2c2bc9a8dc3e14e89
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2019-10-21 15:31:02 +0000

    lngファイルの文字コード変換に失敗する場合があったので修正
    - UTF16leのファイルを出力するときCRの変換が行われていなかった
    - ファイルをバイナリモードで扱うように修正
    - Ttssh2-devel 4318
    
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/branches/unicode_buf@8326 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- installer/utf8_to.pl
+++ installer/utf8_to.pl
@@ -4,8 +4,8 @@ my $code = $ARGV[0];
 my $in = $ARGV[1];
 my $out = $ARGV[2];
 
-open(INP, "<:utf8", $in) or die("error :$! $in");
-open(OUT, ">:encoding($code)", $out) or die("error :$! $out");
+open(INP, "<:raw:utf8", $in) or die("error :$! $in");
+open(OUT, ">:raw:encoding($code)", $out) or die("error :$! $out");
 
 my $line = 1;
 while (<INP>) {
