13e28a64c3d1ba1bad4d1445da7b2284adcbed04
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2020-03-07 15:12:50 +0000

    未使用変数削除、ファイルヘッダの copyright を修正
    - 未使用変数に copyright 文字列が入っていた
    - ファイルヘッダを置き換えて、cpyright をファイルヘッダに入れた
    
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@8577 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- ttpmenu/registry.cpp
+++ ttpmenu/registry.cpp
@@ -1,15 +1,34 @@
+/*
+ * Copyright (C) S.Hayakawa NTT-IT 1998-2002
+ * (C) 2002-2020 TeraTerm Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 //保存先にiniファイルを使用したい場合は、0バイトのファイルでよいのでttpmenu.exeと同じフォルダにttpmenu.iniを用意する
 #define		STRICT
-static char *Registry_id = 
-	"@(#)Copyright (C) NTT-IT 1998  -- registry.cpp --  Ver1.00b1";
-/* ==========================================================================
-	Project Name		: Universal Library
-	Outline				: Registry Function
-	Create				: 1998-02-17(Tue)
-	Update				: 
-	Copyright			: S.Hayakawa				NTT-IT
-	Reference			: 
-   ======1=========2=========3=========4=========5=========6=========7======= */
 #include	"registry.h"
 #include	"tchar.h"
 #include	"stdio.h"
--- ttpmenu/winmisc.cpp
+++ ttpmenu/winmisc.cpp
@@ -1,14 +1,33 @@
+/*
+ * Copyright (C) S.Hayakawa NTT-IT 1998-2002
+ * (C) 2002-2020 TeraTerm Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #define		STRICT
-static char *WinMisc_id = 
-	"@(#)Copyright (C) NTT-IT 1998-2002  -- winmisc.cpp --  Ver1.00b2";
-/* ==========================================================================
-	Project Name		: Universal Library
-	Outline				: WinMisc Function
-	Create				: 1998-02-20(Wed)
-	Update				: 2002-09-25(Wed)
-	Copyright			: S.Hayakawa				NTT-IT
-	Reference			: 
-   ======1=========2=========3=========4=========5=========6=========7======= */
 #include	"winmisc.h"
 
 
