0d7acc350120233d9d3b23a57ec5ee2b585d5fef
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2020-03-18 14:39:19 +0000

    icoファイル保守用スクリプト追加
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@8609 f5f01b69-1e22-0410-acbf-894ab4bd6246

---
+++ teraterm/common/ico/ico_scripts/icon_combine.sh
@@ -0,0 +1,15 @@
+cd teraterm
+icotool -c *.png > ../teraterm.ico
+cd ..
+
+cd teraterm_3d
+icotool -c *.png > ../teraterm_3d.ico
+cd ..
+
+cd teraterm_classic
+icotool -c *.png > ../teraterm_classic.ico
+cd ..
+
+cd tterm16
+icotool -c *.png > ../tterm16.ico
+cd ..
---
+++ teraterm/common/ico/ico_scripts/icon_extract.sh
@@ -0,0 +1,19 @@
+mkdir teraterm
+cd teraterm
+icotool -x ../../../teraterm.ico
+cd ..
+
+mkdir teraterm_3d
+cd teraterm_3d
+icotool -x ../../../teraterm_3d.ico
+cd ..
+
+mkdir teraterm_classic
+cd teraterm_classic
+icotool -x ../../../teraterm_classic.ico
+cd ..
+
+mkdir tterm16
+cd tterm16
+icotool -x ../../../tterm16.ico
+cd ..
