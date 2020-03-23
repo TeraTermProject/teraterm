68f05101e28147a900469ca120d1143ab5f3264c
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2020-03-23 14:16:42 +0000

    アイコンの入れ替え
    - [Ttssh2-devel 4536]
    
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@8615 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- teraterm/common/ico/ico_scripts/icon_combine.sh
+++ teraterm/common/ico/ico_scripts/icon_combine.sh
@@ -13,3 +13,55 @@ cd ..
 cd tterm16
 icotool -c *.png > ../tterm16.ico
 cd ..
+
+cd tek
+icotool -c *.png > ../tek.ico
+cd ..
+
+cd tek16
+icotool -c *.png > ../tek16.ico
+cd ..
+
+cd cygterm
+icotool -c *.png > ../cygterm.ico
+cd ..
+
+cd vt
+icotool -c *.png > ../vt.ico
+cd ..
+
+cd vt_3d
+icotool -c *.png > ../vt_3d.ico
+cd ..
+
+cd vt_classic
+icotool -c *.png > ../vt_classic.ico
+cd ..
+
+cd vt16
+icotool -c *.png > ../vt16.ico
+cd ..
+
+cd ttmacr16
+icotool -c *.png > ../ttmacr16.ico
+cd ..
+
+cd ttmacro
+icotool -c *.png > ../ttmacro.ico
+cd ..
+
+cd ttmacro_3d
+icotool -c *.png > ../ttmacro_3d.ico
+cd ..
+
+cd ttsecure
+icotool -c *.png > ../ttsecure.ico
+cd ..
+
+cd ttsecure_green
+icotool -c *.png >  ../ttsecure_green.ico
+cd ..
+
+cd ttsecure_yellow
+icotool -c *.png > ../ttsecure_yellow.ico
+cd ..
--- teraterm/common/ico/ico_scripts/icon_extract.sh
+++ teraterm/common/ico/ico_scripts/icon_extract.sh
@@ -17,3 +17,68 @@ mkdir tterm16
 cd tterm16
 icotool -x ../../../tterm16.ico
 cd ..
+
+mkdir tek
+cd tek
+icotool -x ../../../tek.ico
+cd ..
+
+mkdir tek16
+cd tek16
+icotool -x ../../../tek16.ico
+cd ..
+
+mkdir cygterm
+cd cygterm
+icotool -x ../../../../../cygterm/cygterm.ico
+cd ..
+
+mkdir vt
+cd vt
+icotool -x ../../../vt.ico
+cd ..
+
+mkdir vt_3d
+cd vt_3d
+icotool -x ../../../vt_3d.ico
+cd ..
+
+mkdir vt_classic
+cd vt_classic
+icotool -x ../../../vt_classic.ico
+cd ..
+
+mkdir vt16
+cd vt16
+icotool -x ../../../vt16.ico
+cd ..
+
+mkdir ttmacr16
+cd ttmacr16
+icotool -x ../../../../ttpmacro/ttmacr16.ico
+cd ..
+
+mkdir ttmacro
+cd ttmacro
+icotool -x ../../../../ttpmacro/ttmacro.ico
+cd ..
+
+mkdir ttmacro_3d
+cd ttmacro_3d
+icotool -x ../../../../ttpmacro/ttmacro_3d.ico
+cd ..
+
+mkdir ttsecure
+cd ttsecure
+icotool -x ../../../../../ttssh2/ttxssh/ttsecure.ico
+cd ..
+
+mkdir ttsecure_green
+cd ttsecure_green
+icotool -x ../../../../../ttssh2/ttxssh/ttsecure_green.ico
+cd ..
+
+mkdir ttsecure_yellow
+cd ttsecure_yellow
+icotool -x ../../../../../ttssh2/ttxssh/ttsecure_yellow.ico
+cd ..
