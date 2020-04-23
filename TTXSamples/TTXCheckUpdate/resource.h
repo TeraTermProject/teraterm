476b08f693d344346f1dc8a74ab02ae440ec24c4
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2020-04-23 14:29:25 +0000

    現在バージョンを表示するようにした
    - リソース名を変更
    - VS2008, VS2019プロジェクトを修正
    
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@8734 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- TTXSamples/TTXCheckUpdate/resource.h
+++ TTXSamples/TTXCheckUpdate/resource.h
@@ -2,11 +2,12 @@
 // Microsoft Visual C++ で生成されたインクルード ファイル。
 // resource.rc で使用
 //
-#define IDD_DIALOG1                     101
-#define IDC_EDIT1                       1001
-#define IDC_COMBO1                      1002
-#define IDC_EDIT2                       1003
-#define IDC_BUTTON2                     1004
+#define IDD_CHECK_UPDATE_DIALOG         101
+#define IDC_DETAIL_EDIT                 1001
+#define IDC_VERSION_DROPDOWN            1002
+#define IDC_URL_EDIT                    1003
+#define IDC_OPEN                        1004
+#define IDC_VERSION_LABEL               1005
 
 // Next default values for new objects
 // 
@@ -14,7 +15,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
--- TTXSamples/TTXCheckUpdate/ttxcheckupdate.c
+++ TTXSamples/TTXCheckUpdate/ttxcheckupdate.c
@@ -41,6 +41,9 @@
 #include "dlglib.h"
 #include "i18n.h"
 #include "layer_for_unicode.h"
+#include "asprintf.h"
+#include "tt-version.h"		// for TT_VERSION_MAJOR, TT_VERSION_MINOR
+#include "svnversion.h"		// for SVNVERSION
 
 #include "resource.h"
 #include "parse.h"
@@ -60,17 +63,38 @@ typedef struct {
 static TInstVar InstVar;
 static TInstVar *pvar;
 
+/**
+ *	ドロップダウンにバージョン情報一覧の一覧をセットする
+ *
+ *	最初に見つかった
+ *	running_version のメジャーバージョンと同じ
+ *	バージョン情報を選択する
+ */
 static int SetDropdown(HWND hDlg, int running_version)
 {
 	const int version_major = running_version / 10000;
+	const int version_minor = running_version % 10000;
+	char *str;
 	int cursor = -1;
 	size_t i;
+	char version_label[32];
 
-	_SendDlgItemMessageW(hDlg, IDC_COMBO1, CB_RESETCONTENT, 0, 0);
+	GetDlgItemText(hDlg, IDC_VERSION_LABEL, version_label, sizeof(version_label));
+#if defined(SVNVERSION)
+	asprintf(&str, "%s (current version teraterm:%d.%d, ttxcheckupdate:%d.%d %s r%d)", version_label, version_major,
+			 version_minor, TT_VERSION_MAJOR, TT_VERSION_MINOR, BRANCH_NAME, SVNVERSION);
+#else
+	asprintf(&str, "%s (current version teraterm:%d.%d, ttxcheckupdate:%d.%d)", version_label, version_major,
+			 version_minor, TT_VERSION_MAJOR, TT_VERSION_MINOR);
+#endif
+	SetDlgItemTextA(hDlg, IDC_VERSION_LABEL, str);
+	free(str);
+
+	_SendDlgItemMessageW(hDlg, IDC_VERSION_DROPDOWN, CB_RESETCONTENT, 0, 0);
 	for (i = 0; i < pvar->versions_count; i++) {
 		const version_one_t *v = &pvar->versions[i];
 		wchar_t *strW = ToWcharU8(v->version_text);
-		_SendDlgItemMessageW(hDlg, IDC_COMBO1, CB_ADDSTRING, 0, (LPARAM)strW);
+		_SendDlgItemMessageW(hDlg, IDC_VERSION_DROPDOWN, CB_ADDSTRING, 0, (LPARAM)strW);
 		free(strW);
 		if (cursor == -1 && v->version_major == version_major) {
 			cursor = (int)i;
@@ -82,23 +106,26 @@ static int SetDropdown(HWND hDlg, int running_version)
 	return cursor;
 }
 
+/**
+ *	version_one_t の情報をダイアログに表示する
+ */
 static void SetTexts(HWND hDlg, const version_one_t *version)
 {
 	const version_one_t *v = version;
 
 	wchar_t *strW = ToWcharU8(v->text);
-	_SetWindowTextW(GetDlgItem(hDlg, IDC_EDIT1), strW);
+	_SetWindowTextW(GetDlgItem(hDlg, IDC_DETAIL_EDIT), strW);
 	free(strW);
 
 	if (v->url == NULL) {
-		EnableWindow(GetDlgItem(hDlg, IDC_BUTTON2), FALSE);
-		EnableWindow(GetDlgItem(hDlg, IDC_EDIT2), FALSE);
-		SetWindowTextA(GetDlgItem(hDlg, IDC_EDIT2), "");
+		EnableWindow(GetDlgItem(hDlg, IDC_OPEN), FALSE);
+		EnableWindow(GetDlgItem(hDlg, IDC_URL_EDIT), FALSE);
+		SetWindowTextA(GetDlgItem(hDlg, IDC_URL_EDIT), "");
 	}
 	else {
-		EnableWindow(GetDlgItem(hDlg, IDC_BUTTON2), TRUE);
-		EnableWindow(GetDlgItem(hDlg, IDC_EDIT2), TRUE);
-		SetWindowTextA(GetDlgItem(hDlg, IDC_EDIT2), v->url);
+		EnableWindow(GetDlgItem(hDlg, IDC_OPEN), TRUE);
+		EnableWindow(GetDlgItem(hDlg, IDC_URL_EDIT), TRUE);
+		SetWindowTextA(GetDlgItem(hDlg, IDC_URL_EDIT), v->url);
 	}
 }
 
@@ -108,8 +135,9 @@ static INT_PTR CALLBACK DlgProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lPara
 	switch (msg) {
 		case WM_INITDIALOG: {
 			int cursor = SetDropdown(hDlg, pvar->ts->RunningVersion);
-			SendDlgItemMessage(hDlg, IDC_COMBO1, CB_SETCURSEL, cursor, 0);
+			SendDlgItemMessage(hDlg, IDC_VERSION_DROPDOWN, CB_SETCURSEL, cursor, 0);
 			SetTexts(hDlg, &pvar->versions[cursor]);
+			PostMessage(hDlg, WM_NEXTDLGCTL, (WPARAM)GetDlgItem(hDlg, IDOK), TRUE);
 			CenterWindow(hDlg, GetParent(hDlg));
 			break;
 		}
@@ -122,14 +150,14 @@ static INT_PTR CALLBACK DlgProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lPara
 				case IDCANCEL | (BN_CLICKED << 16):
 					EndDialog(hDlg, 0);
 					break;
-				case IDC_BUTTON2 | (BN_CLICKED << 16): {
-					int cursor = (int)SendDlgItemMessage(hDlg, IDC_COMBO1, CB_GETCURSEL, 0, 0);
+				case IDC_OPEN | (BN_CLICKED << 16): {
+					int cursor = (int)SendDlgItemMessage(hDlg, IDC_VERSION_DROPDOWN, CB_GETCURSEL, 0, 0);
 					const char *url = pvar->versions[cursor].url;
 					ShellExecuteA(NULL, NULL, url, NULL, NULL, SW_SHOWNORMAL);
 					break;
 				}
-				case IDC_COMBO1 | (CBN_SELCHANGE << 16): {
-					int cursor = (int)SendDlgItemMessage(hDlg, IDC_COMBO1, CB_GETCURSEL, 0, 0);
+				case IDC_VERSION_DROPDOWN | (CBN_SELCHANGE << 16): {
+					int cursor = (int)SendDlgItemMessage(hDlg, IDC_VERSION_DROPDOWN, CB_GETCURSEL, 0, 0);
 					SetTexts(hDlg, &pvar->versions[cursor]);
 					break;
 				}
@@ -147,9 +175,9 @@ static INT_PTR CALLBACK DlgProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lPara
 
 static void ShowDialog(HWND hWnd)
 {
-	const wchar_t *update_info_url = L"https://osdn.dl.osdn.net/storage/g/t/tt/ttssh2/snapshot/teraterm_version.json";
+	const wchar_t *update_info_url_default =
+		L"https://osdn.dl.osdn.net/storage/g/t/tt/ttssh2/snapshot/teraterm_version.json";
 	const wchar_t *agent_base = L"teraterm_updatechecker";
-	wchar_t buf[256];
 	wchar_t agent[128];
 	int result_mb;
 	char *json_raw_ptr;
@@ -158,20 +186,21 @@ static void ShowDialog(HWND hWnd)
 	size_t json_size;
 	char *json_ptr;
 	const char *UILanguageFile = pvar->ts->UILanguageFile;
-	wchar_t UIMsg[MAX_UIMSG];
+	wchar_t update_info_url[MAX_UIMSG];
+	static const TTMessageBoxInfoW info = {
+		"TTXCheckUpdate",
+		NULL, L"Tera Term",
+		"MSG_CHECKUPDATE", L"Do you want to check update?\n%s" };
 
-	/* ファイルを取得してもok? */
-	GetI18nStrW("TTXCheckUpdate", "MSG_CHECKUPDATE", UIMsg, _countof(UIMsg),
-				L"Do you want to check update?\n"
-				L"  %s\n",
+	/* 更新情報を取得してもok? */
+	GetI18nStrW("TTXCheckUpdate", "JSON_URL", update_info_url, _countof(update_info_url), update_info_url_default,
 				UILanguageFile);
-	swprintf(buf, _countof(buf), UIMsg, update_info_url);
-	result_mb = _MessageBoxW(hWnd, buf, L"Tera Term", MB_YESNO | MB_ICONEXCLAMATION);
+	result_mb = TTMessageBoxW(hWnd, &info, MB_YESNO | MB_ICONEXCLAMATION, UILanguageFile, update_info_url);
 	if (result_mb == IDNO) {
 		return;
 	}
 
-	/* ファイル取得、'\0'を追加する→ json文字列を作成 */
+	/* 更新情報取得、'\0'を追加する→ json文字列を作成 */
 	swprintf(agent, _countof(agent), L"%s_%d", agent_base, pvar->ts->RunningVersion);
 	result_bool = GetContent(update_info_url, agent, (void**)&json_raw_ptr, &json_raw_size);
 	if (!result_bool) {
@@ -187,7 +216,7 @@ static void ShowDialog(HWND hWnd)
 	json_raw_ptr = NULL;
 	json_ptr[json_size - 1] = '\0';
 
-	/* jsonをパースする */
+	/* jsonをパースする, versionsに情報が入る */
 	pvar->versions = ParseJson(json_ptr, &pvar->versions_count);
 	if (pvar->versions == NULL) {
 		_MessageBoxW(hWnd, L"parse error?", L"Tera Term", MB_OK | MB_ICONEXCLAMATION);
@@ -197,7 +226,7 @@ static void ShowDialog(HWND hWnd)
 	/* ダイアログを出す */
 	SetDialogFont(pvar->ts->DialogFontName, pvar->ts->DialogFontPoint, pvar->ts->DialogFontCharSet,
 				  pvar->ts->UILanguageFile, "Tera Term", "DLG_TAHOMA_FONT");
-	TTDialogBoxParam(pvar->hInst, MAKEINTRESOURCE(IDD_DIALOG1), hWnd, DlgProc, (LPARAM)pvar);
+	TTDialogBoxParam(pvar->hInst, MAKEINTRESOURCE(IDD_CHECK_UPDATE_DIALOG), hWnd, DlgProc, (LPARAM)pvar);
 
 	/* 終了 */
 	free(json_ptr);
