97a6a33aec40b5dafe1d97162a455695da9d9143
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2020-05-16 16:42:16 +0000

    win16_lopen() で OF_READWRITE は使用しなくなったので削除、ファイルヘッダを追加
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@8782 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- teraterm/common/win16api.c
+++ teraterm/common/win16api.c
@@ -1,3 +1,31 @@
+/*
+ * Copyright (C) 2018-2020 TeraTerm Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <windows.h>
 #include <assert.h>
 
@@ -56,12 +84,6 @@ HANDLE win16_lopenW(const wchar_t *FileName, int iReadWrite)
 							 GENERIC_WRITE, FILE_SHARE_WRITE, NULL,
 							 OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 		break;
-	case OF_READWRITE:
-		// read/write (teratermではttpmacro/ttl.c内の1箇所のみで使用されている
-		handle = CreateFileW(FileName,
-							 GENERIC_WRITE|GENERIC_READ, FILE_SHARE_WRITE, NULL,
-							 OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-		break;
 	default:
 		assert(FALSE);
 		handle = INVALID_HANDLE_VALUE;
@@ -89,12 +111,6 @@ HANDLE win16_lopen(const char *FileName, int iReadWrite)
 		// write
 		handle = CreateFileA(FileName,
 							 GENERIC_WRITE, FILE_SHARE_WRITE, NULL,
-							 OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-		break;
-	case OF_READWRITE:
-		// read/write (teratermではttpmacro/ttl.c内の1箇所のみで使用されている
-		handle = CreateFileA(FileName,
-							 GENERIC_WRITE|GENERIC_READ, FILE_SHARE_WRITE, NULL,
 							 OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 		break;
 	default:
