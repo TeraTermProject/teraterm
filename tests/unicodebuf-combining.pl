b42c8cb3f9c5b27cd0a8312d24ccdec55b1fcebe
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2020-07-02 14:56:21 +0000

    結合文字+行末wrap処理テスト
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@8821 f5f01b69-1e22-0410-acbf-894ab4bd6246

---
+++ tests/unicodebuf-combining.pl
@@ -0,0 +1,37 @@
+﻿# 結合文字、行末処理テスト
+
+use utf8;
+use Time::HiRes 'usleep';
+
+binmode STDOUT, ":utf8";
+local $| = 1;
+
+$sleep_time = 100*1000;
+
+# U+0041 + U+0302 (A + ^ → Â)
+for ($i = 0; $i < 80; $i++) {
+	print "\N{U+0041}";
+	usleep($sleep_time);
+	print "\N{U+0302}";
+	usleep($sleep_time);
+#	print "\N{U+0300}";
+#	usleep($sleep_time);
+}
+
+# U+307B + U+309A (ほ + ゚ = ぽ)
+for ($i = 0; $i < 40; $i++) {
+	print "\N{U+307B}";
+	usleep($sleep_time);
+	if ((($i+1) % 40) == 0){ sleep(1); }
+	print "\N{U+309A}";
+	usleep($sleep_time);
+	if ((($i+1) % 40) == 0){ sleep(1); }
+}
+
+# U+0061 U+0302 ( a+^ → â )
+for ($i = 0; $i < 80; $i++) {
+	print "\N{U+0061}";
+	usleep($sleep_time);
+	print "\N{U+0302}";
+	usleep($sleep_time);
+}
