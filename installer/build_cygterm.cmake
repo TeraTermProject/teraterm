4b441e4933178b82d1088888f269d21840d75d8f
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2020-07-13 15:31:02 +0000

    cmakeビルド install時に、cygtermをビルドするようにした
    - cygwinがある場合はビルドする
    - ない場合は install に cygterm は含まれない
    
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@8849 f5f01b69-1e22-0410-acbf-894ab4bd6246

---
+++ installer/build_cygterm.cmake
@@ -0,0 +1,21 @@
+﻿
+if(NOT DEFINED CYGWIN_PATH)
+  set(CYGWIN_PATH "C:/cygwin64/bin")
+  if (NOT EXISTS ${CYGWIN_PATH})
+    set(CYGWIN_PATH "C:/cygwin/bin")
+    if (NOT EXISTS ${CYGWIN_PATH})
+      message(FATAL_ERROR "Not found cygwin")
+    endif()
+  endif()
+endif()
+
+message("build_cygterm.cmake")
+message("CMAKE_CURRENT_SOURCE_DIR=${CMAKE_CURRENT_SOURCE_DIR}")
+message("CMAKE_SOURCE_DIR=${CMAKE_SOURCE_DIR}")
+message("CMAKE_CURRENT_LIST_DIR=${CMAKE_CURRENT_LIST_DIR}")
+message("CYGWIN_PATH=${CYGWIN_PATH}")
+set(ENV{PATH} ${CYGWIN_PATH})
+execute_process(
+  COMMAND make all
+  WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/../cygterm
+  )
