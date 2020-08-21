560e9104b9105d592a3029f125d034fca87c9b11
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2020-08-21 15:23:41 +0000

    ファイル全体のコメントを追加, メモリリークチェック追加
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@8915 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- teraterm/teraterm/checkeol.cpp
+++ teraterm/teraterm/checkeol.cpp
@@ -1,5 +1,34 @@
+/*
+ * Copyright (C) 2020 TeraTerm Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
+#define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
+#include <crtdbg.h>
 #include <windows.h>
 
 #include "checkeol.h"
@@ -40,8 +69,7 @@ void CheckEOLClear(CheckEOLData_t *self)
 CheckEOLRet CheckEOLCheck(CheckEOLData_t *self, unsigned int u32)
 {
    	// 入力が改行(CR or LF)の場合、
-	// 改行の種類(CR or LF or CR+LF)を自動で判定して
-	// OutputLogNewLine() で改行を出力する
+	// 改行の種類(CR or LF or CR+LF)を自動で判定する
 	//		入力    CR hold     改行出力   	CR hold 変更
    	// 		+-------+-----------+-----------+------------
 	//		CR      なし        しない		セットする
