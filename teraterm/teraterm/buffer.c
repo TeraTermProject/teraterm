ab1232686fe6f4d8444557fb7e9ce5fad4532c81
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2021-01-02 12:10:30 +0000

    URL強調無限ループ
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@9121 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- teraterm/teraterm/buffer.c
+++ teraterm/teraterm/buffer.c
@@ -2371,7 +2371,8 @@ static void mark_url_line_w(int cur_x, int cur_y)
 	int sx_i = sx;
 	int sy_i = sy;
 	int i;
-	for (i=0; i<1000000; i++) {
+	//for (i=0; i<1000000; i++) {
+	for (;;) {
 		int sx_match_s, sx_match_e;
 		int sy_match_s, sy_match_e;
 		BOOL match;
