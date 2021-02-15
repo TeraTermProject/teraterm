d252502bd38574f636d963b737d7f7a02abf1af2
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2021-02-15 15:02:25 +0000

    innosetup, unrar のダウンロードURLを変更
    - 特定バージョンへのURLに変更
      - innosetup
        - 修正前 https://jrsoftware.org/download.php/is.exe
        - 修正後 https://files.jrsoftware.org/is/6/innosetup-6.0.5.exe
      - unrar
        - 修正前 http://gnuwin32.sourceforge.net/downlinks/unrar-bin-zip.php
        - 修正後 https://sourceforge.net/projects/gnuwin32/files/unrar/3.4.3/unrar-3.4.3-bin.zip/download
    - innosetup.cmakeを実行するごとにツールを展開し直すようにした
    - innosetupを最新に入れ替えできず,6.0.5のまま
      - innosetup
        - 6.0.5 -> 6.1.2
        - 6.1で圧縮方法が変わったらしい
      - innounp
        - 0.49 -> 0.50
        - 6.1に対応
        - RAR v5 で圧縮されている
      - unrar
        - 3.4.3
        - エラーが出る
          - innounp050.rar is not RAR archive
        - RAR v5 に対応していないようだ
    
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@9154 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- buildtools/innosetup.cmake
+++ buildtools/innosetup.cmake
@@ -1,44 +1,66 @@
 ﻿# cmake -P innosetup.cmake
 
 # unrar
+set(UNRAR_ZIP "unrar-3.4.3-bin.zip")
+set(UNRAR_URL "https://sourceforge.net/projects/gnuwin32/files/unrar/3.4.3/${UNRAR_ZIP}/download")
+set(UNRAR_HASH "b7c3cc0865bfc32cb9e7e2017c91c5d3601eb97b3bcd37f42c36d9c62f1ae46c")
 file(MAKE_DIRECTORY "download/unrar")
 file(DOWNLOAD
-  http://gnuwin32.sourceforge.net/downlinks/unrar-bin-zip.php
-  download/unrar/unrar-3.4.3-bin.zip
-  EXPECTED_HASH SHA256=b7c3cc0865bfc32cb9e7e2017c91c5d3601eb97b3bcd37f42c36d9c62f1ae46c
+  ${UNRAR_URL}
+  download/unrar/${UNRAR_ZIP}
+  EXPECTED_HASH SHA256=${UNRAR_HASH}
   SHOW_PROGRESS
   )
+if(EXISTS "unrar")
+    file(REMOVE_RECURSE "unrar")
+endif()
 file(MAKE_DIRECTORY "unrar")
 execute_process(
-  COMMAND ${CMAKE_COMMAND} -E tar xvf ../download/unrar/unrar-3.4.3-bin.zip
+  COMMAND ${CMAKE_COMMAND} -E tar xvf ../download/unrar/${UNRAR_ZIP}
   WORKING_DIRECTORY "unrar"
   )
 
 # innounp
+#set(INNOUNP_RAR "innounp050.rar")
+#set(INNOUNP_URL "https://sourceforge.net/projects/innounp/files/innounp/innounp%200.50/${INNOUNP_RAR}/download")
+#set(INNOUNP_HASH "1d8837540ccc15d98245a1c73fd08f404b2a7bdfe7dc9bed2fdece818ff6df67")
+set(INNOUNP_RAR "innounp049.rar")
+set(INNOUNP_URL "https://sourceforge.net/projects/innounp/files/innounp/innounp%200.49/${INNOUNP_RAR}/download")
+set(INNOUNP_HASH "4a636ff9213928a919717d0fab28e525fb992d9bb5f2d4a9ab2fd97132690741")
 file(MAKE_DIRECTORY "download/innounp")
 file(DOWNLOAD
-  https://sourceforge.net/projects/innounp/files/innounp/innounp%200.49/innounp049.rar/download
-  download/innounp/innounp049.rar
-  EXPECTED_HASH SHA256=4a636ff9213928a919717d0fab28e525fb992d9bb5f2d4a9ab2fd97132690741
+  ${INNOUNP_URL}
+  download/innounp/${INNOUNP_RAR}
+  EXPECTED_HASH SHA256=${INNOUNP_HASH}
   SHOW_PROGRESS
   )
+if(EXISTS "innounp")
+    file(REMOVE_RECURSE "innounp")
+endif()
 file(MAKE_DIRECTORY "innounp")
 execute_process(
-  COMMAND ../unrar/bin/unrar.exe x ../download/innounp/innounp049.rar
+  COMMAND ../unrar/bin/unrar.exe x ../download/innounp/${INNOUNP_RAR}
   WORKING_DIRECTORY "innounp"
   )
 
 # innosetup 6
+#set(INNOSETUP_EXE "innosetup-6.1.2.exe")
+#set(INNOSETUP_HASH "a3ce1c40ef9c71a92691aaff0f413f530c8c9e3c766be481bc63ca7cc74e35e7")
+set(INNOSETUP_EXE "innosetup-6.0.5.exe")
+set(INNOSETUP_HASH "ae6823b523df87e9441789e51845434e4e0e70aac0b88afe80f94f20f4b98acb")
 file(MAKE_DIRECTORY "download/innosetup6")
 file(DOWNLOAD
-  https://jrsoftware.org/download.php/is.exe
-  download/innosetup6/innosetup-6.0.5.exe
-  EXPECTED_HASH SHA256=ae6823b523df87e9441789e51845434e4e0e70aac0b88afe80f94f20f4b98acb
+  https://files.jrsoftware.org/is/6/${INNOSETUP_EXE}
+  download/innosetup6/${INNOSETUP_EXE}
+  EXPECTED_HASH SHA256=${INNOSETUP_HASH}
   SHOW_PROGRESS
   )
+if(EXISTS "innosetup6")
+    file(REMOVE_RECURSE "innosetup6")
+endif()
 file(MAKE_DIRECTORY "innosetup6")
 execute_process(
-  COMMAND ../innounp/innounp.exe -x ../download/innosetup6/innosetup-6.0.5.exe
+  COMMAND ../innounp/innounp.exe -x ../download/innosetup6/${INNOSETUP_EXE}
   WORKING_DIRECTORY "innosetup6"
   )
 file(RENAME "innosetup6/{app}" innosetup6/bin)
