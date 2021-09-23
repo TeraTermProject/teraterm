3ba13dd69af19ce233d27e013d6f204b3a9b186a
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2021-09-23 15:54:06 +0000

    IDEから gobal,doxygen プロジェクトをビルドできなかった
    - IDEからドキュメントが作れなかった
    - カレントディレクトリが移動していてもビルドできるようにした
    
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@9450 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- doc_internal/global.cmake
+++ doc_internal/global.cmake
@@ -1,13 +1,13 @@
 ﻿
-find_program(HTAGS htags)
-message("HTAGS=${HTAGS}")
-
 file(REMOVE_RECURSE global)
 
 execute_process(
   COMMAND ${CMAKE_COMMAND} -P gtags_update.cmake
   WORKING_DIRECTORY ".."
   )
+
+find_program(HTAGS htags)
+message("HTAGS=${HTAGS}")
 
 execute_process(
   COMMAND ${HTAGS} -ans --tabs 4 -F
