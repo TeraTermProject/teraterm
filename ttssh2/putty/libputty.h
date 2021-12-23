5eb8438d3209d33c4731e86b8021f47b7b06d2cf
nmaya <5668936+nmaya@users.noreply.github.com>
2021-12-23 16:48:04 +0000

    PuTTY 0.76 にアップデート
    - 展開して使うソースを PuTTY 0.76 に変更
    - コピーして使うソースも PuTTY 0.76 にアップデート
    
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/branches/putty_0_76@9626 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- ttssh2/putty/libputty.h
+++ ttssh2/putty/libputty.h
@@ -25,23 +25,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-// PuTTY is copyright 1997-2007 Simon Tatham.
-
-// pageant.h
-// 本当は pageant.h を include 出来るようにする方がいいのかもしれないけれど
-// 関数のプロトタイプ宣言もここにあるので取りあえずここで。
-#define AGENT_MAX_MSGLEN 8192
-
-// エラー応答用
-#define SSH_AGENT_FAILURE_MSG "\x00\x00\x00\x01\x05"
-
-// MISC.C
-extern void safefree(void *);
-
-// WINDOWS\WINPGNTC.C
-extern int agent_exists(void);
-extern void *agent_query(void *in, int inlen, void **out, int *outlen,
-                       void (*callback)(void *, void *, int), void *callback_ctx);
+// PuTTY is copyright 1997-2021 Simon Tatham.
 
 int putty_get_ssh2_keylist(unsigned char **keylist);
 void *putty_sign_ssh2_key(unsigned char *pubkey,
@@ -57,7 +41,16 @@ void *putty_hash_ssh1_challenge(unsigned char *pubkey,
                                 int *outlen);
 int putty_get_ssh1_keylen(unsigned char *key, int maxlen);
 const char *putty_get_version();
+void putty_agent_query_synchronous(void *in, int inlen, void **out, int *outlen);
 BOOL putty_agent_exists();
 
-static void *get_keylist1(int *length);
-static void *get_keylist2(int *length);
+// エラー応答用
+#define SSH_AGENT_FAILURE_MSG "\x00\x00\x00\x01\x05"
+
+// pageant.h
+#define AGENT_MAX_MSGLEN 262144
+
+// puttymen.h
+//   memory.c
+extern void safefree(void *);
+
