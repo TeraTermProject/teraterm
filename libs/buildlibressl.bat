6205c0e1c558201d15aaafd51a9fb1943d675b60
nmaya <5668936+nmaya@users.noreply.github.com>
2021-12-26 13:27:43 +0000

    すでに LibreSSL がビルドされている時に、再びビルドしないように修正
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/branches/libressl_3_4_2@9637 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- libs/buildlibressl.bat
+++ libs/buildlibressl.bat
@@ -3,6 +3,8 @@ rem LibreSSL‚Ìƒrƒ‹ƒh
 cd libressl
 
 
+if exist "CMakeCache.txt" goto end
+
 cmake -G "Visual Studio 16 2019" -A Win32
 perl -e "open(IN,'CMakeCache.txt');while(<IN>){s|=/MD|=/MT|;print $_;}close(IN);" > CMakeCache.txt.tmp
 move /y CMakeCache.txt.tmp CMakeCache.txt
@@ -14,5 +16,6 @@ devenv /build Debug LibreSSL.sln /project crypto /projectconfig Debug
 devenv /build Release LibreSSL.sln /project crypto /projectconfig Release
 
 
+:end
 cd ..
 exit /b 0
