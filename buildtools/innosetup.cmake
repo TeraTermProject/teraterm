91c572f48ebca5d22a4e03d16064551e4b83a199
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2022-01-08 16:17:39 +0000

    cmakeビルド,innosetupを6.2.0へ更新
    - 入れ替え前は 6.0.5 を使用していた
      - self-extract形式
      - ローカルで展開するためには unpacker(innounp)が必要
    - innounp 0.50 に入れ替え
      - 0.50 から innosetup 6.1.2以降のunpackに対応
      - RAR v5 でアーカイブされている
      - 入れ替え前の unnounp 0.49 は RAR v4 でアーカイブされていた
    - RAR v5 に対応した unrarに入れ替え
      - 入れ替え前の unrar は RAR v4 までしか対応していなかった
    
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@9682 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- buildtools/innosetup.cmake
+++ buildtools/innosetup.cmake
@@ -1,9 +1,9 @@
 ﻿# cmake -P innosetup.cmake
 
 # unrar
-set(UNRAR_ZIP "unrar-3.4.3-bin.zip")
-set(UNRAR_URL "https://sourceforge.net/projects/gnuwin32/files/unrar/3.4.3/${UNRAR_ZIP}/download")
-set(UNRAR_HASH "b7c3cc0865bfc32cb9e7e2017c91c5d3601eb97b3bcd37f42c36d9c62f1ae46c")
+set(UNRAR_ZIP "unrarw32.exe")
+set(UNRAR_URL "https://www.rarlab.com/rar/unrarw32.exe")
+set(UNRAR_HASH "63dac3b0e3faa90f85e234e0608ce7db94dd6a43b12793c71895b3ee35ff7755")
 file(MAKE_DIRECTORY "download/unrar")
 file(DOWNLOAD
   ${UNRAR_URL}
@@ -16,17 +16,14 @@ if(EXISTS "unrar")
 endif()
 file(MAKE_DIRECTORY "unrar")
 execute_process(
-  COMMAND ${CMAKE_COMMAND} -E tar xvf ../download/unrar/${UNRAR_ZIP}
+  COMMAND ../download/unrar/${UNRAR_ZIP} /s
   WORKING_DIRECTORY "unrar"
   )
 
 # innounp
-#set(INNOUNP_RAR "innounp050.rar")
-#set(INNOUNP_URL "https://sourceforge.net/projects/innounp/files/innounp/innounp%200.50/${INNOUNP_RAR}/download")
-#set(INNOUNP_HASH "1d8837540ccc15d98245a1c73fd08f404b2a7bdfe7dc9bed2fdece818ff6df67")
-set(INNOUNP_RAR "innounp049.rar")
-set(INNOUNP_URL "https://sourceforge.net/projects/innounp/files/innounp/innounp%200.49/${INNOUNP_RAR}/download")
-set(INNOUNP_HASH "4a636ff9213928a919717d0fab28e525fb992d9bb5f2d4a9ab2fd97132690741")
+set(INNOUNP_RAR "innounp050.rar")
+set(INNOUNP_URL "https://sourceforge.net/projects/innounp/files/innounp/innounp%200.50/${INNOUNP_RAR}/download")
+set(INNOUNP_HASH "1d8837540ccc15d98245a1c73fd08f404b2a7bdfe7dc9bed2fdece818ff6df67")
 file(MAKE_DIRECTORY "download/innounp")
 file(DOWNLOAD
   ${INNOUNP_URL}
@@ -39,15 +36,13 @@ if(EXISTS "innounp")
 endif()
 file(MAKE_DIRECTORY "innounp")
 execute_process(
-  COMMAND ../unrar/bin/unrar.exe x ../download/innounp/${INNOUNP_RAR}
+  COMMAND ../unrar/UnRAR.exe x ../download/innounp/${INNOUNP_RAR}
   WORKING_DIRECTORY "innounp"
   )
 
 # innosetup 6
-#set(INNOSETUP_EXE "innosetup-6.1.2.exe")
-#set(INNOSETUP_HASH "a3ce1c40ef9c71a92691aaff0f413f530c8c9e3c766be481bc63ca7cc74e35e7")
-set(INNOSETUP_EXE "innosetup-6.0.5.exe")
-set(INNOSETUP_HASH "ae6823b523df87e9441789e51845434e4e0e70aac0b88afe80f94f20f4b98acb")
+set(INNOSETUP_EXE "innosetup-6.2.0.exe")
+set(INNOSETUP_HASH "2459da3c0a67346bc43a9732d96929877d04f53b1d4c56e61be64e3b5f34d5cf")
 file(MAKE_DIRECTORY "download/innosetup6")
 file(DOWNLOAD
   https://files.jrsoftware.org/is/6/${INNOSETUP_EXE}
