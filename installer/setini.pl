84556b9b3454391ee2828f4532999ec67e031d4e
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2022-02-17 13:14:34 +0000

    インストーラの Locale 設定箇所を削除
    - Locale は使用しなくなった(r9313)
    
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@9748 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- installer/setini.pl
+++ installer/setini.pl
@@ -3,7 +3,6 @@
 open INI, $ARGV[0];
 while(<INI>){
 	s/^(Language\s*=).*$/$1Japanese/;
-	s/^(Locale\s*=).*$/$1japanese/;
 #	s/^(VTFont\s*=).*$/$1Terminal,0,-19,128/;
 	s/^(VTFont\s*=).*$/$1ÇlÇr ÉSÉVÉbÉN,0,-16,128/;
 	s/^(TEKFont\s*=).*$/$1Terminal,0,8,128/;
