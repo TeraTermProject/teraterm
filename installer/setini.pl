5d4d0f7ce673cd9d999db547a6adcef0dd6f911c
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2022-02-22 14:11:03 +0000

    setini.pl を Unicodeベースに変更
    - setini.pl を utf-8 に変更
    - 入力ファイル、出力ファイルの文字コードは cp932
      - 変更前と同じ
    - どの環境でも出力iniファイルの改行コードはCRLFとした
    
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@9769 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- installer/setini.pl
+++ installer/setini.pl
@@ -1,10 +1,20 @@
-#!/usr/bin/perl
+﻿#!/usr/bin/perl
+# 日本語環境用に teraterm.ini を設定する
 
-open INI, $ARGV[0];
+use strict;
+use warnings;
+use utf8;
+
+my $in_file = $ARGV[0];
+
+my $vtfont = 'ＭＳ ゴシック,0,-16,128';
+#my $vtfont = 'Terminal,0,-19,128';
+
+binmode STDOUT, ":crlf:encoding(cp932)";
+open(INI, '<:crlf:encoding(cp932)', $in_file);
 while(<INI>){
 	s/^(Language\s*=).*$/$1Japanese/;
-#	s/^(VTFont\s*=).*$/$1Terminal,0,-19,128/;
-	s/^(VTFont\s*=).*$/$1�l�r �S�V�b�N,0,-16,128/;
+	s/^(VTFont\s*=).*$/$1$vtfont/;
 	s/^(TEKFont\s*=).*$/$1Terminal,0,8,128/;
 	s/^(TCPPort\s*=).*$/${1}22/;
 	print;
