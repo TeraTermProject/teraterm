b3bab7b073e15fb8af594dd8ef85b452d5854566
nmaya <5668936+nmaya@users.noreply.github.com>
2022-05-23 23:47:13 +0000

    /ssh-icon= に TTSSH オリジナルのアイコンを指定できるようにした
    4-stable の r9952 と同じ修正
    
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@9953 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- ttssh2/ttxssh/resource.h
+++ ttssh2/ttxssh/resource.h
@@ -19,7 +19,8 @@
 #define IDD_SSHUPDATE_HOSTKEY           111
 #define IDI_SECURETT_GREEN              113
 #define IDI_DROPDOWN                    114
-#define IDI_SECURETT_FLAT               115
+#define IDI_SECURETT_CLASSIC            115
+#define IDI_SECURETT_FLAT               116
 #define IDD_HOSTDLG                     600
 #define IDC_HOSTTCPIP                   601
 #define IDD_SSHDIFFERENTKEY             601
