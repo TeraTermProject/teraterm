dfcb9119344b834e75754d405ad96f8b5d46a2a6
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2022-10-23 15:16:47 +0000

    cyglaunch と同等のbatファイルを追加
    - cmd,powershell,wsl を msys2term 経由で起動するbatファイル
      - cygwinなし、msys2あり環境でつかえる
    - TODO
      - cyglaunchへ組み込み
      - ttregへ組み込み
    
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@10335 f5f01b69-1e22-0410-acbf-894ab4bd6246

---
+++ cygwin/cyglaunch/cyglaunch_wsl_msys2term.bat
@@ -0,0 +1,3 @@
+set DIR=%~dp0
+set PATH=c:\msys64\usr\bin;%PATH%
+start "" "%DIR%\msys2term.exe" -t "%DIR%\ttermpro.exe %%s %%d /E /KR=UTF8 /KT=UTF8 /nossh /VTICON=TTERM" -s "c:/windows/system32/wsl.exe"
