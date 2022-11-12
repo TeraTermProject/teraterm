fc3bf554fd64383c83a8d70330bd4e5e2f97faca
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2022-11-12 14:44:20 +0000

    タブをスペースに変換
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@10360 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- libs/buildsfmt.cmake
+++ libs/buildsfmt.cmake
@@ -6,52 +6,52 @@
 ####
 if(("${CMAKE_BUILD_TYPE}" STREQUAL "") AND ("${CMAKE_CONFIGURATION_TYPE}" STREQUAL ""))
   if("${CMAKE_GENERATOR}" MATCHES "Visual Studio")
-	# multi-configuration
-	execute_process(
-	  COMMAND ${CMAKE_COMMAND}
-	  -DCMAKE_GENERATOR=${CMAKE_GENERATOR}
-	  -DCMAKE_CONFIGURATION_TYPE=Release
-	  -DCMAKE_TOOLCHAIN_FILE=${CMAKE_SOURCE_DIR}/VSToolchain.cmake
-	  -DARCHITECTURE=${ARCHITECTURE}
-	  -P buildsfmt.cmake
-	  )
-	execute_process(
-	  COMMAND ${CMAKE_COMMAND}
-	  -DCMAKE_GENERATOR=${CMAKE_GENERATOR}
-	  -DCMAKE_CONFIGURATION_TYPE=Debug
-	  -DCMAKE_TOOLCHAIN_FILE=${CMAKE_SOURCE_DIR}/VSToolchain.cmake
-	  -DARCHITECTURE=${ARCHITECTURE}
-	  -P buildsfmt.cmake
-	  )
-	return()
+    # multi-configuration
+    execute_process(
+      COMMAND ${CMAKE_COMMAND}
+      -DCMAKE_GENERATOR=${CMAKE_GENERATOR}
+      -DCMAKE_CONFIGURATION_TYPE=Release
+      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_SOURCE_DIR}/VSToolchain.cmake
+      -DARCHITECTURE=${ARCHITECTURE}
+      -P buildsfmt.cmake
+      )
+    execute_process(
+      COMMAND ${CMAKE_COMMAND}
+      -DCMAKE_GENERATOR=${CMAKE_GENERATOR}
+      -DCMAKE_CONFIGURATION_TYPE=Debug
+      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_SOURCE_DIR}/VSToolchain.cmake
+      -DARCHITECTURE=${ARCHITECTURE}
+      -P buildsfmt.cmake
+      )
+    return()
   elseif(("$ENV{MSYSTEM}" MATCHES "MINGW") OR ("${CMAKE_COMMAND}" MATCHES "mingw"))
-	# mingw on msys2
-	if("${CMAKE_BUILD_TYPE}" STREQUAL "")
-	  set(CMAKE_BUILD_TYPE Release)
-	endif()
+    # mingw on msys2
+    if("${CMAKE_BUILD_TYPE}" STREQUAL "")
+      set(CMAKE_BUILD_TYPE Release)
+    endif()
   elseif("${CMAKE_GENERATOR}" MATCHES "Unix Makefiles")
-	# mingw
-	# single-configuration
-	if("${CMAKE_TOOLCHAIN_FILE}" STREQUAL "")
-	  set(CMAKE_TOOLCHAIN_FILE "${CMAKE_SOURCE_DIR}/../mingw.toolchain.cmake")
-	endif()
-	if("${CMAKE_BUILD_TYPE}" STREQUAL "")
-	  set(CMAKE_BUILD_TYPE Release)
-	endif()
+    # mingw
+    # single-configuration
+    if("${CMAKE_TOOLCHAIN_FILE}" STREQUAL "")
+      set(CMAKE_TOOLCHAIN_FILE "${CMAKE_SOURCE_DIR}/../mingw.toolchain.cmake")
+    endif()
+    if("${CMAKE_BUILD_TYPE}" STREQUAL "")
+      set(CMAKE_BUILD_TYPE Release)
+    endif()
   elseif("${CMAKE_GENERATOR}" MATCHES "NMake Makefiles")
-	# VS nmake
-	# single-configuration
-	if("${CMAKE_TOOLCHAIN_FILE}" STREQUAL "")
-	  set(CMAKE_TOOLCHAIN_FILE "${CMAKE_SOURCE_DIR}/VSToolchain.cmake")
-	endif()
-	if("${CMAKE_BUILD_TYPE}" STREQUAL "")
-	  set(CMAKE_BUILD_TYPE Release)
-	endif()
+    # VS nmake
+    # single-configuration
+    if("${CMAKE_TOOLCHAIN_FILE}" STREQUAL "")
+      set(CMAKE_TOOLCHAIN_FILE "${CMAKE_SOURCE_DIR}/VSToolchain.cmake")
+    endif()
+    if("${CMAKE_BUILD_TYPE}" STREQUAL "")
+      set(CMAKE_BUILD_TYPE Release)
+    endif()
   else()
-	# single-configuration
-	if("${CMAKE_BUILD_TYPE}" STREQUAL "")
-	  set(CMAKE_BUILD_TYPE Release)
-	endif()
+    # single-configuration
+    if("${CMAKE_BUILD_TYPE}" STREQUAL "")
+      set(CMAKE_BUILD_TYPE Release)
+    endif()
   endif()
 endif()
 
@@ -78,8 +78,8 @@ endif()
 if(NOT EXISTS ${SRC_DIR}/README.txt)
 
   file(DOWNLOAD
-	${SRC_URL}
-	${DOWN_DIR}/${SRC_ARC}
+    ${SRC_URL}
+    ${DOWN_DIR}/${SRC_ARC}
     EXPECTED_HASH SHA256=${SRC_ARC_HASH_SHA256}
     SHOW_PROGRESS
     )
@@ -114,29 +114,29 @@ if(NOT EXISTS ${SRC_DIR}/SFMT_version_for_teraterm.h)
 endif()
 if(NOT EXISTS ${SRC_DIR}/CMakeLists.txt)
   file(WRITE "${SRC_DIR}/CMakeLists.txt"
-	"cmake_minimum_required(VERSION 2.4.4)\n"
-	"project(SFMT C)\n"
-	"\n"
-	"if(MSVC)\n"
-	"  set(CMAKE_DEBUG_POSTFIX \"d\")\n"
-	"endif()\n"
-	"\n"
-	"add_library(\n"
-	"  sfmt STATIC\n"
-	"  SFMT.c\n"
-	"  )\n"
-	"\n"
-	"install(\n"
-	"  TARGETS sfmt\n"
-	"  ARCHIVE DESTINATION \${CMAKE_INSTALL_PREFIX}/lib\n"
-	"  )\n"
-	"install(\n"
-	"  FILES\n"
+    "cmake_minimum_required(VERSION 2.4.4)\n"
+    "project(SFMT C)\n"
+    "\n"
+    "if(MSVC)\n"
+    "  set(CMAKE_DEBUG_POSTFIX \"d\")\n"
+    "endif()\n"
+    "\n"
+    "add_library(\n"
+    "  sfmt STATIC\n"
+    "  SFMT.c\n"
+    "  )\n"
+    "\n"
+    "install(\n"
+    "  TARGETS sfmt\n"
+    "  ARCHIVE DESTINATION \${CMAKE_INSTALL_PREFIX}/lib\n"
+    "  )\n"
+    "install(\n"
+    "  FILES\n"
     "    SFMT.h SFMT-params.h SFMT-params19937.h\n"
     "    SFMT_version_for_teraterm.h\n"
-	"  DESTINATION \${CMAKE_INSTALL_PREFIX}/include\n"
-	"  )\n"
-	)
+    "  DESTINATION \${CMAKE_INSTALL_PREFIX}/include\n"
+    "  )\n"
+    )
 endif()
 
 ########################################
@@ -148,51 +148,51 @@ if("${CMAKE_GENERATOR}" MATCHES "Visual Studio")
   ######################################## multi configuration
 
   if(NOT "${ARCHITECTURE}" STREQUAL "")
-	set(CMAKE_A_OPTION -A ${ARCHITECTURE})
+    set(CMAKE_A_OPTION -A ${ARCHITECTURE})
   endif()
   execute_process(
-	COMMAND ${CMAKE_COMMAND} ${SRC_DIR} -G ${CMAKE_GENERATOR} ${CMAKE_A_OPTION}
-	-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
-	-DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
-	${TOOLCHAINFILE}
-	WORKING_DIRECTORY ${BUILD_DIR}
-	RESULT_VARIABLE rv
-	)
+    COMMAND ${CMAKE_COMMAND} ${SRC_DIR} -G ${CMAKE_GENERATOR} ${CMAKE_A_OPTION}
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+    -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
+    ${TOOLCHAINFILE}
+    WORKING_DIRECTORY ${BUILD_DIR}
+    RESULT_VARIABLE rv
+    )
   if(NOT rv STREQUAL "0")
-	message(FATAL_ERROR "cmake generate fail ${rv}")
+    message(FATAL_ERROR "cmake generate fail ${rv}")
   endif()
 
   execute_process(
-	COMMAND ${CMAKE_COMMAND} --build . --config ${CMAKE_CONFIGURATION_TYPE} --target install
-	WORKING_DIRECTORY ${BUILD_DIR}
-	RESULT_VARIABLE rv
-	)
+    COMMAND ${CMAKE_COMMAND} --build . --config ${CMAKE_CONFIGURATION_TYPE} --target install
+    WORKING_DIRECTORY ${BUILD_DIR}
+    RESULT_VARIABLE rv
+    )
   if(NOT rv STREQUAL "0")
-	message(FATAL_ERROR "cmake install fail ${rv}")
+    message(FATAL_ERROR "cmake install fail ${rv}")
   endif()
 
 else()
   ######################################## single configuration
 
   execute_process(
-	COMMAND ${CMAKE_COMMAND} ${SRC_DIR} -G ${CMAKE_GENERATOR}
-	-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-	-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
-	-DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
-	WORKING_DIRECTORY ${BUILD_DIR}
-	RESULT_VARIABLE rv
-	)
+    COMMAND ${CMAKE_COMMAND} ${SRC_DIR} -G ${CMAKE_GENERATOR}
+    -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+    -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
+    WORKING_DIRECTORY ${BUILD_DIR}
+    RESULT_VARIABLE rv
+    )
   if(NOT rv STREQUAL "0")
-	message(FATAL_ERROR "cmake build fail ${rv}")
+    message(FATAL_ERROR "cmake build fail ${rv}")
   endif()
 
   execute_process(
-	COMMAND ${CMAKE_COMMAND} --build . --target install
-	WORKING_DIRECTORY ${BUILD_DIR}
-	RESULT_VARIABLE rv
-	)
+    COMMAND ${CMAKE_COMMAND} --build . --target install
+    WORKING_DIRECTORY ${BUILD_DIR}
+    RESULT_VARIABLE rv
+    )
   if(NOT rv STREQUAL "0")
-	message(FATAL_ERROR "cmake install fail ${rv}")
+    message(FATAL_ERROR "cmake install fail ${rv}")
   endif()
 
 endif()
--- libs/buildzlib.cmake
+++ libs/buildzlib.cmake
@@ -5,52 +5,52 @@
 ####
 if(("${CMAKE_BUILD_TYPE}" STREQUAL "") AND ("${CMAKE_CONFIGURATION_TYPE}" STREQUAL ""))
   if("${CMAKE_GENERATOR}" MATCHES "Visual Studio")
-	# multi-configuration
-	execute_process(
-	  COMMAND ${CMAKE_COMMAND}
-	  -DCMAKE_GENERATOR=${CMAKE_GENERATOR}
-	  -DCMAKE_CONFIGURATION_TYPE=Release
-	  -DCMAKE_TOOLCHAIN_FILE=${CMAKE_SOURCE_DIR}/VSToolchain.cmake
-	  -DARCHITECTURE=${ARCHITECTURE}
-	  -P buildzlib.cmake
-	  )
-	execute_process(
-	  COMMAND ${CMAKE_COMMAND}
-	  -DCMAKE_GENERATOR=${CMAKE_GENERATOR}
-	  -DCMAKE_CONFIGURATION_TYPE=Debug
-	  -DCMAKE_TOOLCHAIN_FILE=${CMAKE_SOURCE_DIR}/VSToolchain.cmake
-	  -DARCHITECTURE=${ARCHITECTURE}
-	  -P buildzlib.cmake
-	  )
-	return()
+    # multi-configuration
+    execute_process(
+      COMMAND ${CMAKE_COMMAND}
+      -DCMAKE_GENERATOR=${CMAKE_GENERATOR}
+      -DCMAKE_CONFIGURATION_TYPE=Release
+      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_SOURCE_DIR}/VSToolchain.cmake
+      -DARCHITECTURE=${ARCHITECTURE}
+      -P buildzlib.cmake
+      )
+    execute_process(
+      COMMAND ${CMAKE_COMMAND}
+      -DCMAKE_GENERATOR=${CMAKE_GENERATOR}
+      -DCMAKE_CONFIGURATION_TYPE=Debug
+      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_SOURCE_DIR}/VSToolchain.cmake
+      -DARCHITECTURE=${ARCHITECTURE}
+      -P buildzlib.cmake
+      )
+    return()
   elseif(("$ENV{MSYSTEM}" MATCHES "MINGW") OR ("${CMAKE_COMMAND}" MATCHES "mingw"))
-	# mingw on msys2
-	if("${CMAKE_BUILD_TYPE}" STREQUAL "")
-	  set(CMAKE_BUILD_TYPE Release)
-	endif()
+    # mingw on msys2
+    if("${CMAKE_BUILD_TYPE}" STREQUAL "")
+      set(CMAKE_BUILD_TYPE Release)
+    endif()
   elseif("${CMAKE_GENERATOR}" MATCHES "Unix Makefiles")
-	# mingw
-	# single-configuration
-	if("${CMAKE_TOOLCHAIN_FILE}" STREQUAL "")
-	  set(CMAKE_TOOLCHAIN_FILE "${CMAKE_SOURCE_DIR}/../mingw.toolchain.cmake")
-	endif()
-	if("${CMAKE_BUILD_TYPE}" STREQUAL "")
-	  set(CMAKE_BUILD_TYPE Release)
-	endif()
+    # mingw
+    # single-configuration
+    if("${CMAKE_TOOLCHAIN_FILE}" STREQUAL "")
+      set(CMAKE_TOOLCHAIN_FILE "${CMAKE_SOURCE_DIR}/../mingw.toolchain.cmake")
+    endif()
+    if("${CMAKE_BUILD_TYPE}" STREQUAL "")
+      set(CMAKE_BUILD_TYPE Release)
+    endif()
   elseif("${CMAKE_GENERATOR}" MATCHES "NMake Makefiles")
-	# VS nmake
-	# single-configuration
-	if("${CMAKE_TOOLCHAIN_FILE}" STREQUAL "")
-	  set(CMAKE_TOOLCHAIN_FILE "${CMAKE_SOURCE_DIR}/VSToolchain.cmake")
-	endif()
-	if("${CMAKE_BUILD_TYPE}" STREQUAL "")
-	  set(CMAKE_BUILD_TYPE Release)
-	endif()
+    # VS nmake
+    # single-configuration
+    if("${CMAKE_TOOLCHAIN_FILE}" STREQUAL "")
+      set(CMAKE_TOOLCHAIN_FILE "${CMAKE_SOURCE_DIR}/VSToolchain.cmake")
+    endif()
+    if("${CMAKE_BUILD_TYPE}" STREQUAL "")
+      set(CMAKE_BUILD_TYPE Release)
+    endif()
   else()
-	# single-configuration
-	if("${CMAKE_BUILD_TYPE}" STREQUAL "")
-	  set(CMAKE_BUILD_TYPE Release)
-	endif()
+    # single-configuration
+    if("${CMAKE_BUILD_TYPE}" STREQUAL "")
+      set(CMAKE_BUILD_TYPE Release)
+    endif()
   endif()
 endif()
 
@@ -106,51 +106,51 @@ if("${CMAKE_GENERATOR}" MATCHES "Visual Studio")
   ######################################## multi configuration
 
   if(NOT "${ARCHITECTURE}" STREQUAL "")
-	set(CMAKE_A_OPTION -A ${ARCHITECTURE})
+    set(CMAKE_A_OPTION -A ${ARCHITECTURE})
   endif()
   execute_process(
-	COMMAND ${CMAKE_COMMAND} ${SRC_DIR} -G ${CMAKE_GENERATOR} ${CMAKE_A_OPTION}
-	-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
-	-DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
-	${TOOLCHAINFILE}
-	WORKING_DIRECTORY ${BUILD_DIR}
-	RESULT_VARIABLE rv
-	)
+    COMMAND ${CMAKE_COMMAND} ${SRC_DIR} -G ${CMAKE_GENERATOR} ${CMAKE_A_OPTION}
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+    -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
+    ${TOOLCHAINFILE}
+    WORKING_DIRECTORY ${BUILD_DIR}
+    RESULT_VARIABLE rv
+    )
   if(NOT rv STREQUAL "0")
-	message(FATAL_ERROR "cmake generate fail ${rv}")
+    message(FATAL_ERROR "cmake generate fail ${rv}")
   endif()
 
   execute_process(
-	COMMAND ${CMAKE_COMMAND} --build . --config ${CMAKE_CONFIGURATION_TYPE} --target install
-	WORKING_DIRECTORY ${BUILD_DIR}
-	RESULT_VARIABLE rv
-	)
+    COMMAND ${CMAKE_COMMAND} --build . --config ${CMAKE_CONFIGURATION_TYPE} --target install
+    WORKING_DIRECTORY ${BUILD_DIR}
+    RESULT_VARIABLE rv
+    )
   if(NOT rv STREQUAL "0")
-	message(FATAL_ERROR "cmake install fail ${rv}")
+    message(FATAL_ERROR "cmake install fail ${rv}")
   endif()
 
 else()
   ######################################## single configuration
 
   execute_process(
-	COMMAND ${CMAKE_COMMAND} ${SRC_DIR} -G ${CMAKE_GENERATOR}
-	-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-	-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
-	-DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
-	WORKING_DIRECTORY ${BUILD_DIR}
-	RESULT_VARIABLE rv
-	)
+    COMMAND ${CMAKE_COMMAND} ${SRC_DIR} -G ${CMAKE_GENERATOR}
+    -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+    -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
+    WORKING_DIRECTORY ${BUILD_DIR}
+    RESULT_VARIABLE rv
+    )
   if(NOT rv STREQUAL "0")
-	message(FATAL_ERROR "cmake build fail ${rv}")
+    message(FATAL_ERROR "cmake build fail ${rv}")
   endif()
 
   execute_process(
-	COMMAND ${CMAKE_COMMAND} --build . --target install
-	WORKING_DIRECTORY ${BUILD_DIR}
-	RESULT_VARIABLE rv
-	)
+    COMMAND ${CMAKE_COMMAND} --build . --target install
+    WORKING_DIRECTORY ${BUILD_DIR}
+    RESULT_VARIABLE rv
+    )
   if(NOT rv STREQUAL "0")
-	message(FATAL_ERROR "cmake install fail ${rv}")
+    message(FATAL_ERROR "cmake install fail ${rv}")
   endif()
 
 endif()
