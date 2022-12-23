09b3fa1c31fe9de2a6ad485d56f832fc2683ebf5
nmaya <5668936+nmaya@users.noreply.github.com>
2022-12-23 13:59:45 +0000

    PuTTY 0.78 への更新テスト
    現在ビルドは通らない
    
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/branches/putty_0_78@10433 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- ttssh2/putty/putty-import.c
+++ ttssh2/putty/putty-import.c
@@ -244,108 +244,6 @@ int rsa_ssh1_public_blob_len(ptrlen data)
     return src->pos;
 }
 
-// from windows/winmiscs.c (PuTTY 0.76)
-/*
- * Windows implementation of smemclr (see misc.c) using SecureZeroMemory.
- */
-void smemclr(void *b, size_t n) {
-    if (b && n > 0)
-        SecureZeroMemory(b, n);
-}
-
-// from be_misc.c (PuTTY 0.76)
-void log_proxy_stderr(Plug *plug, ProxyStderrBuf *psb,
-                      const void *vdata, size_t len)
-{
-    const char *data = (const char *)vdata;
-
-    /*
-     * This helper function allows us to collect the data written to a
-     * local proxy command's standard error in whatever size chunks we
-     * happen to get from its pipe, and whenever we have a complete
-     * line, we pass it to plug_log.
-     *
-     * (We also do this when the buffer in psb fills up, to avoid just
-     * allocating more and more memory forever, and also to keep Event
-     * Log lines reasonably bounded in size.)
-     *
-     * Prerequisites: a plug to log to, and a ProxyStderrBuf stored
-     * somewhere to collect any not-yet-output partial line.
-     */
-
-    while (len > 0) {
-        /*
-         * Copy as much data into psb->buf as will fit.
-         */
-        assert(psb->size < lenof(psb->buf));
-        size_t to_consume = lenof(psb->buf) - psb->size;
-        if (to_consume > len)
-            to_consume = len;
-        memcpy(psb->buf + psb->size, data, to_consume);
-        data += to_consume;
-        len -= to_consume;
-        psb->size += to_consume;
-
-        /*
-         * Output any full lines in psb->buf.
-         */
-        size_t pos = 0;
-        while (pos < psb->size) {
-            char *nlpos = memchr(psb->buf + pos, '\n', psb->size - pos);
-            if (!nlpos)
-                break;
-
-            /*
-             * Found a newline in the buffer, so we can output a line.
-             */
-            size_t endpos = nlpos - psb->buf;
-            while (endpos > pos && (psb->buf[endpos-1] == '\n' ||
-                                    psb->buf[endpos-1] == '\r'))
-                endpos--;
-            char *msg = dupprintf(
-                "proxy: %.*s", (int)(endpos - pos), psb->buf + pos);
-            plug_log(plug, PLUGLOG_PROXY_MSG, NULL, 0, msg, 0);
-            sfree(msg);
-
-            pos = nlpos - psb->buf + 1;
-            assert(pos <= psb->size);
-        }
-
-        /*
-         * If the buffer is completely full and we didn't output
-         * anything, then output the whole thing, flagging it as a
-         * truncated line.
-         */
-        if (pos == 0 && psb->size == lenof(psb->buf)) {
-            char *msg = dupprintf(
-                "proxy (partial line): %.*s", (int)psb->size, psb->buf);
-            plug_log(plug, PLUGLOG_PROXY_MSG, NULL, 0, msg, 0);
-            sfree(msg);
-
-            pos = psb->size = 0;
-        }
-
-        /*
-         * Now move any remaining data up to the front of the buffer.
-         */
-        size_t newsize = psb->size - pos;
-        if (newsize)
-            memmove(psb->buf, psb->buf + pos, newsize);
-        psb->size = newsize;
-
-        /*
-         * And loop round again if there's more data to be read from
-         * our input.
-         */
-    }
-}
-
-// from be_misc.c (PuTTY 0.76)
-void psb_init(ProxyStderrBuf *psb)
-{
-    psb->size = 0;
-}
-
 // from windows/winpgnt.c (PuTTY 0.76)
 void noise_ultralight(NoiseSourceId id, unsigned long data)
 {
@@ -371,63 +269,69 @@ void modalfatalbox(const char *fmt, ...)
     sfree(title);
 }
 
-// from misc.c (PuTTY 0.76)
-void out_of_memory(void)
-{
-    modalfatalbox("Out of memory");
-}
-
 
 /*
+agent_query_synchronous()@aqsync.c ... include
+
+rsa_ssh1_public_blob_len()
+	mp_free()@crypto\mpint.c ... include
+		smemclr()@utils\smemclr.c ... include
+
 putty_get_ssh1_keylen()@libputty.c
 	rsa_ssh1_public_blob_len()@sshrsa.c ... import
 
-memory.c / mpint.c / utils.c
-	smemclr()@windows/winmiscs.c ... import
+strbuf_new()@utils\strbuf.c ... include
 
 safemalloc()@memory.c
-	out_of_memory()@misc.c ... import
-		modalfatalbox()@windows/window.c ... import
+	out_of_memory()@utils\out_of_memory.c ... include
+		modalfatalbox()@windows\window.c ... import
 
-wm_copydata_agent_query()@windows\winpgnt.c
-	got_advapi()@windows\winsecur.c ... include winsecur.c
+wm_copydata_agent_query()@windows\agent-client.c ... include
+	got_advapi()@windows\utils\security.c ... include
 
-agent_named_pipe_name()@windows\winpgnt.c
-	get_username()@windows\winmisc.c ... include winmisc.c
+agent_connect()@windows\agent-client.c
+	new_named_pipe_client()@windows\named-pipe-client.c ... include
+		connect_to_named_pipe()@windows\named-pipe-client.c
+			win_strerror()@windows\utils\win_strerror.c ... include
+		new_error_socket_consume_string()@errsock.c ... include
+		make_handle_socket()@windows\handle-socket.c ... include
+			bufchain_init()@utils\bufchain.c ... include
+			handle_input_new()@windows\handle-io.c ... include
+				ensure_ready_event_setup()@@windows\handle-io.c
+					add_handle_wait()@windows\handle-wait.c
+			handle_output_new()@windows\handle-io.c
+			psb_init()@utils\log_proxy_stderr.c ... include
+			handle_stderr()@windows\handle-socket.c
+				log_proxy_stderr()@utils\log_proxy_stderr.c
+			handle_sentdata()@windows\handle-socket.c
+				plug_closing_system_error()@windows\network.c ... ?
 
-agent_connect()@windows\winpgnt.c
-	new_named_pipe_client()@winnpc.c ... include winnpc.c
-		connect_to_named_pipe()@winnpc.c
-		new_error_socket_consume_string()@errsock.c ... include errsock.c
-		make_handle_socket()@winsocket.c ... include winsocket.c
-			handle_input_new()@winhandl.c ... include winhandl.c
-			handle_output_new()@winhandl.c
-			psb_init()@be_misc.c ... import
-			handle_stderr()@winhsock.c
-				log_proxy_stderr()@be_misc.c ... import
+agent_named_pipe_name()@windows\agent-client.c
+	get_username()@windows\utils\get_username.c ... include
+		load_system32_dll()@windows\utils\load_system32_dll.c ... include
+			dupcat()@utils\dupcat.c ... include
+			get_system_dir()@windows\utils\get_system_dir.c ... include
+	capi_obfuscate_string()@windows\utils\cryptoapi.c ... include
+		dupstr()@utils\dupstr.c
+		ssh_sha256@crypto\sha256-select.c ... include
 
-agent_named_pipe_name()@windows\winpgnt.c
-	capi_obfuscate_string()@windows\wincapi.c ... include wincapi.c
-		ssh_sha256@sshsh256.c ... include sshsh256.c
+HandleSocket_sockvt@windows\handle-socket.c
+	sk_handle_set_frozen()@windows\handle-socket.c
+		queue_toplevel_callback()@callback.c ... include
+	sk_handle_close()@windows\handle-socket.c
+		delete_callbacks_for_context()@callback.c
+			ssh_sha256_sw@ssh.h ... ?
+		sk_addr_free()@windows\network.c ... ?
 
-handle_got_event()@windows\winhandl.c
-	noise_ultralight()@windows/winpgnt.c
+tree234@utils\tree234.c ... include
 
-sk_handle_set_frozen()@windows\winhsock.c
-	queue_toplevel_callback()@callback.c ... include callback.c
-sk_handle_close()@windows\winhsock.c
-	delete_callbacks_for_context()@callback.c
+BinarySink_put_fmtv()@marshal.c
+	burnstr()@utils\burnstr.c ... include
+	dupprintf()@utils\dupprintf.c ... include
 
-winmisc.c refers to tree234 ... include tree234.c
+putty_get_version()@libputty.c
+	ver@version.c ... include
 
-
-
-if include be_misc.c ...
-backend_socket_log()@be_misc.c
-	sk_getaddr()@windows\winnet.c
-	sk_addr_needs_port()@windows\winnet.c
-	conf_get_int()@conf.c
-	logevent()@logging.c
 
 if include sshrsa.c ...
 rsa_components()@sshrsa.c
