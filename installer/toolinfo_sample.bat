77fc829d9b43905e6baed6efb332d38e8c64a82f
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2023-01-10 13:18:02 +0000

    toolinfo.bat のサンプルを修正
    - Visual Studio 2022 に変更
    - Cygwin を buildtools\cygwin64 に設定する場合を追加
    
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@10469 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- installer/toolinfo_sample.bat
+++ installer/toolinfo_sample.bat
@@ -7,9 +7,10 @@ rem set PATH=
 rem 各ツールへのパスなどを指定する
 rem set PERL_PATH=C:\Strawberry\perl\bin
 rem set SVN_PATH=C:\Program Files (x86)\Subversion\bin
-rem set VS_BASE=C:\Program Files (x86)\Microsoft Visual Studio\2019
+rem set VS_BASE=C:\Program Files\Microsoft Visual Studio\2022
 rem set CMAKE_PATH=C:\Program Files\CMake\bin
 rem set CURL=C:\Windows\System32\curl.exe
+rem set CYGWIN_PATH=%~dp0..\buildtools\cygwin64\bin
 rem set CYGWIN_PATH=C:\cygwin64\bin
 rem set INNO_SETUP="C:\Program Files (x86)\Inno Setup 6\iscc.exe"
 
