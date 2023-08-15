6712d578ae06e83e80de7c33061ff3f39c27a083
nmaya <5668936+nmaya@users.noreply.github.com>
2023-08-15 13:46:20 +0000

    cmake を見つけられないのを修正
    git-svn-id: https://svn.osdn.net/svnroot/ttssh2/trunk@10845 f5f01b69-1e22-0410-acbf-894ab4bd6246

--- ci_scripts/build_appveyor_release_bat.bat
+++ ci_scripts/build_appveyor_release_bat.bat
@@ -1,4 +1,7 @@
 set CUR=%~dp0
+
 cd /d %CUR%..
 call ci_scripts\install.bat
+
+cd /d %CUR%..
 call installer\release.bat 2
--- ci_scripts/install.bat
+++ ci_scripts/install.bat
@@ -1,12 +1,18 @@
 echo %~dp0\install.bat
+
+cd /d %~dp0
+
 setlocal
 if "%CMAKE_COMMAND%" == "" (
    call find_cmake.bat
 )
 
-cd %~dp0
 "%CMAKE_COMMAND%" -P ../buildtools/install_cygwin.cmake
 
 if exist c:\msys64\usr\bin\pacman.exe (
   c:\msys64\usr\bin\pacman.exe  -S --noconfirm --needed cmake
 )
+
+rem ‚±‚±‚Å cd ‚µ‚Ä‚àˆÚ“®‚³‚ê‚È‚¢
+rem install.bat ‚ğŒÄ‚Ño‚µ‚½‘¤‚Å–ß‚µ‚Ä‚à‚ç‚¤
+rem cd ..
