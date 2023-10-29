af8440bd3e39ec1526616ab22907682a5971705b
nmaya <5668936+nmaya@users.noreply.github.com>
2023-10-30 08:43:52 +0900

    fix typo #11

--- ci_scripts/notify.bat
+++ ci_scripts/notify.bat
@@ -6,9 +6,9 @@ set result=%1
 pushd %~dp0
 
 if "%APPVEYOR%" == "" goto pass_discord
-if "%WEBHOOK_URL%" == "" goto pass_discoard
+if "%WEBHOOK_URL%" == "" goto pass_discord
 if "%CYGWIN_PATH%" == "" call %~dp0..\buildtools\find_cygwin.bat
-if "%CYGWIN_PATH%" == "" goto pass_discoard
+if "%CYGWIN_PATH%" == "" goto pass_discord
 
 rem NEED perl-JSON,perl-LWP-Protocol-https packages
 %CYGWIN_PATH%\perl.exe notify_discord.pl %result%
