2b0a09d2fd22973340dac9544632a055e3f62212
nmaya <5668936+nmaya@users.noreply.github.com>
2024-02-24 19:03:53 +0900

    fix typo

---
+++ installer/toolinfo.bat
@@ -0,0 +1,20 @@
+rem toolinfo.bat
+rem  sample
+
+rem 必要ならpathの設定や削除を行う
+rem set PATH=
+
+rem 各ツールへのパスなどを指定する
+rem set PERL_PATH=C:\Strawberry\perl\bin
+rem set SVN_PATH=C:\Program Files (x86)\Subversion\bin
+rem set GIT_PATH=C:\Program Files\Git\cmd
+GIT_PATH=C:\freesoft\Git-2.42.0.2-64-bit\cmd
+rem set VS_BASE=C:\Program Files\Microsoft Visual Studio\2022
+rem set CMAKE_PATH=C:\Program Files\CMake\bin
+rem set CURL=C:\Windows\System32\curl.exe
+rem set CYGWIN_PATH=%~dp0..\buildtools\cygwin64\bin
+rem set CYGWIN_PATH=C:\cygwin64\bin
+rem set INNO_SETUP="C:\Program Files (x86)\Inno Setup 6\iscc.exe"
+
+rem release.bat への指示
+rem set NOPAUSE=0
