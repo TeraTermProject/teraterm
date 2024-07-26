dbf030f1ef8a37278ee10754b1ab16846f398069
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2024-07-26 23:48:23 +0900

    Generalタブ時に正しいヘルプページが表示されるよう修正
    - b7d357b

--- teraterm/common/helpid.h
+++ teraterm/common/helpid.h
@@ -157,6 +157,7 @@
 #define HlpMenuSetupThemeEditor             43025
 #define HlpMenuSetupAdditionalKeyboard      43026
 #define HlpMenuSetupAdditionalMouse         43027
+#define HlpMenuSetupAdditionalGeneral       43028
 #define HlpMenuSetupSave                    43013
 #define HlpMenuSetupRestore                 43014
 #define HlpMenuSetupDir                     43016
