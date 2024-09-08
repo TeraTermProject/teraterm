5b83fb6f727400f27a88ff189e64389fc6fea8c7
nmaya <5668936+nmaya@users.noreply.github.com>
2024-09-08 15:38:40 +0900

    bump up the version to 5.3

--- installer/teraterm.iss
+++ installer/teraterm.iss
@@ -1,6 +1,6 @@
 ﻿#define AppName "Tera Term"
 #ifndef AppVer
-#define AppVer "5.3-RC2"
+#define AppVer "5.3"
 #endif
 ;#define VerSubStr
 ;#define OutputSubStr
--- teraterm/common/tt-version.h
+++ teraterm/common/tt-version.h
@@ -30,8 +30,8 @@
 
 #define TT_VERSION_MAJOR             5
 #define TT_VERSION_MINOR             3
-#define TT_VERSION_SUBSTR            "RC2"
-//#undef TT_VERSION_SUBSTR
+//#define TT_VERSION_SUBSTR            "RC2"
+#undef TT_VERSION_SUBSTR
 // SUBSTR が不要な時は undef する
 //  使用例 "dev", "RC", "RC2"
 
