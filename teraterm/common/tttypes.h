59c004c1fe023a084feb793fd08a16368387ea24
nmaya <5668936+nmaya@users.noreply.github.com>
2024-09-23 00:24:29 +0900

    Beep が Visual Bell のときの flash 時間を設定できるようにした #333

--- teraterm/common/tttypes.h
+++ teraterm/common/tttypes.h
@@ -616,6 +616,7 @@ struct tttset {
 	WORD BracketedControlOnly;
 	wchar_t *ViewlogEditorW;
 	wchar_t *ViewlogEditorArg;
+	int BeepVBellWait;
 
 	// Experimental
 	BYTE ExperimentalTreeProprtySheetEnable;
