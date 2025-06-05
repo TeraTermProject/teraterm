658745504564363083303ea0b627782523ac7cca
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2025-06-05 16:20:19 +0900

    コード署名を行うスクリプトを追加 #316

---
+++ ci_scripts/sign_signpath/README.md
@@ -0,0 +1,94 @@
+﻿# SignPath によるコード署名
+
+- [SignPath][SignPath]
+
+## 概要
+
+TeraTermポータブル版zipから署名済みTeraTermポータブル版zipとインストーラーを作成します。
+
+スクリプト(sign_signpath.ps1)を実行すると
+[SignPath を利用するためのモジュール](https://about.signpath.io/documentation/powershell/)が
+インストールされ、署名済みファイルが作成されます。
+
+## スクリプトの使い方
+
+準備
+
+- ポータブル版zip
+  - 例 `teraterm-5.5.0.zip`
+- [SignPath][SignPath] の API token
+  - `SIGNPATH_API_TOKEN` 環境変数にセットしておく
+  - `set SIGNPATH_API_TOKEN=AD50kRDEwv...` (cmd)
+  - `$env:SIGNPATH_API_TOKEN = ""LKF765DSFlkj...."""` (PowerShell)
+  - `export SIGNPATH_API_TOKEN=JKLBLID.......` (bash)
+
+実行
+
+(例)
+
+    pwsh sign_signpath.ps1 ../../installer/Output/teraterm-5.5.0.zip
+
+出力
+
+- 署名済みポータブル版zip
+  - 例 `teraterm-5.5.0_signed.zip`
+- 署名済みインストーラー
+  - 例 `teraterm-5.5.0_signed.exe`
+
+## 実行例
+
+```
+> ver
+
+Microsoft Windows [Version 10.0.19045.5917]
+
+> pwsh -v
+PowerShell 7.5.1
+
+> set SIGNPATH_API_TOKEN=AD50kRD.........
+
+> pwsh sign_signpath.ps1 ../../installer/Output/teraterm-5.5.0-dev-20250699_999999-d25685f9e-noname-snapshot.zip
+pwd: "C:\...\teraterm\ci_scripts\sign_signpath"
+Source directory: C:\...\teraterm\ci_scripts\sign_signpath/../..
+zipFilename(Portable unsigned zip): "../../installer/Output/teraterm-5.5.0-dev-20250699_999999-d25685f9e-noname-snapshot.zip"
+version: "5.5.0-dev-20250699_999999-d25685f9e-noname-snapshot"
+
+    Directory: C:\...\teraterm\ci_scripts\sign_signpath
+ :
+ :
+フォルダ名 : teraterm-5.5.0-dev-20250605_162051-d25685f9e-noname-snapshot
+SHA256 hash: EC8A0E32C6FFD09AB2AE8BCB6FA630E191D30A7A36CF1A834B0CC522BD28DBA9
+Submitted signing request at 'https://app.signpath.io/api/v1/78e41f48-55fc-4f33-8ea5-08ac32f65ac8/SigningRequests/4db21f6f-76e4-4a95-9d56-ef98a85be74e'
+Checking status... InProgress
+Checking status... InProgress
+Checking status... Completed
+Downloading signed artifact...
+Downloaded signed artifact and saved at 'C:\...\teraterm\ci_scripts\sign_signpath\teraterm_signed.zip'
+4db21f6f-76e4-4a95-9d56-ef98a85be74e
+ :
+ :
+Inno Setup 6 Command-Line Compiler
+Copyright (C) 1997-2025 Jordan Russell. All rights reserved.
+Portions Copyright (C) 2000-2025 Martijn Laan. All rights reserved.
+Portions Copyright (C) 2001-2004 Alex Yackimoff. All rights reserved.
+ :
+ :
+Successful compile (5.125 sec). Resulting Setup program filename is:
+C:\...\teraterm\ci_scripts\sign_signpath\teraterm-5.5.0-dev-20250699_999999-d25685f9e-noname-snapshot_unsigned.exe
+Requesting signing for teraterm-5.5.0-dev-20250699_999999-d25685f9e-noname-snapshot.exe
+SHA256 hash: 37A3F1AEA3428170421B0E4249ABE65C8B2296A4F5BCEADA95CD54947726A674
+Submitted signing request at 'https://app.signpath.io/api/v1/78e41f48-55fc-4f33-8ea5-08ac32f65ac8/SigningRequests/7c7795fd-af3f-4177-b9f5-e71e7b0a2bcf'
+Checking status... Completed
+Downloading signed artifact...
+Downloaded signed artifact and saved at 'C:\...\teraterm\ci_scripts\sign_signpath\teraterm-5.5.0-dev-20250699_999999-d25685f9e-noname-snapshot_signed.exe'
+7c7795fd-af3f-4177-b9f5-e71e7b0a2bcf
+
+> dir /B *.zip *.exe
+teraterm-5.5.0-dev-20250699_999999-d25685f9e-noname-snapshot_signed.zip
+teraterm-5.5.0-dev-20250699_999999-d25685f9e-noname-snapshot_signed.exe
+
+>
+
+```
+
+[SignPath]:https://signpath.io/
---
+++ ci_scripts/sign_signpath/sign_signpath.ps1
@@ -0,0 +1,239 @@
+﻿#
+# SignPathを使ってTera Termのポータブルzipとインストーラを署名
+#
+# - 準備
+#   - 環境変数 SIGNPATH_API_TOKEN に SignPath の API Token をセットしておく
+# - 実行(例)
+#   - pwsh sign_signpath.ps1 ../../installer/Output/teraterm-5.5.0.zip
+#
+param(
+    [string]$zipFilename
+)
+
+if (-not $zipFilename) {
+    throw "ex  sign_signpath.ps1 teraterm-5.5.0.zip"
+    #$zipFilename = Get-ItemProperty -Path ".\tera*.zip" | Sort-Object -Descending | Select-Object -First 1
+}
+$API_TOKEN = $env:SIGNPATH_API_TOKEN
+if (-not $API_TOKEN) {
+    Write-Output "SIGNPATH_API_TOKEN is not set."
+    Write-Output "Set SIGNPATH_API_TOKEN environment variable to your SignPath API token."
+    Write-Output "For example, in PowerShell: `$env:SIGNPATH_API_TOKEN = ""LKF765DSFlkj...."""
+    # string "Bearer " is unnecessary.
+    throw "SIGNPATH_API_TOKEN is not set."
+}
+
+$SOURCE_DIR = Join-Path $PSScriptRoot "../..";
+$ORGANIZATION_ID = "78e41f48-55fc-4f33-8ea5-08ac32f65ac8"
+$PROJECT_SLUG = "teraterm"
+$SIGNING_POLICY_SLUG = "test-signing"
+$ISCC = Join-Path $SOURCE_DIR "buildtools/innosetup6/ISCC.exe"
+$version = $zipFilename -replace '^.*teraterm-', '' -replace '\.zip$', ''
+
+# debug
+$enableSignZip = $true
+$enableSignInstaller = $true
+
+#Set-Location "$PSScriptRoot"
+Write-Output "pwd: ""$PSScriptRoot"""
+Write-Output "Source directory: $SOURCE_DIR"
+Write-Output "zipFilename(Portable unsigned zip): ""$zipFilename"""
+Write-Output "version: ""$version"""
+#Write-Output "API_TOKEN: ""$API_TOKEN"""
+
+#
+# $zipFilename を $destination フォルダへ展開する
+#
+# $zipFilename 内のファイルは、フォルダの下に入っていることを想定している
+#
+#   teraterm-5.5.0-dev-20250526_234540-e7d9a3fb0-noname-snapshot/ttermpro.exe
+#   teraterm-5.5.0-dev-20250526_234540-e7d9a3fb0-noname-snapshot/keycode.exe
+#     :
+#     v
+#   $destination/ttermpro.exe
+#   $destination/keycode.exe
+#     :
+#
+function ExtractTeraTermPortableZip() {
+    param (
+        [string]$zipFilename,
+        [string]$destination
+    )
+
+    # 展開先
+    if (Test-Path $destination) {
+        Write-Error "remove ""$destination"""
+        exit 1
+    }
+    mkdir $destination
+
+    # 一時的に展開するフォルダ
+    $unzip_tmp = "unzip_tmp_{0}" -f ([System.Guid]::NewGuid().ToString("N"))
+
+    Expand-Archive -Path $zipFilename -DestinationPath $unzip_tmp -Force
+
+    $dir = Get-ChildItem -Path $unzip_tmp
+    Write-Output "フォルダ名 : $($dir.Name)"
+
+    # $dir以下のファイルとフォルダをすべて$destinationへ移動
+    foreach ($item in Get-ChildItem -Path $dir) {
+        Move-Item -Path $item.FullName -Destination $destination -Force
+    }
+
+    Remove-Item -Path $unzip_tmp -Recurse -Force
+}
+
+#
+# ポータブルzip($unsignedZipFilename) を署名して $signedZipFilename へ書き込む
+#  zip内の指定ファイルを署名する
+#  どのファイルを署名するかは SignPathサイト内で設定している
+#    Project / Artifact Configuration / teraterm_portable
+#
+function SignZip {
+    param (
+        [string]$unsignedZipFilename,
+        [string]$signedZipFilename,
+        [string]$API_TOKEN
+    )
+
+    if ($enableSignZip) {
+        Submit-SigningRequest `
+          -InputArtifactPath $unsignedZipFilename `
+          -ApiToken $API_TOKEN `
+          -ArtifactConfigurationSlug "teraterm_portable" `
+          -OrganizationId $ORGANIZATION_ID `
+          -ProjectSlug $PROJECT_SLUG `
+          -SigningPolicySlug $SIGNING_POLICY_SLUG `
+          -OutputArtifactPath $signedZipFilename `
+          -Force `
+          -WaitForCompletion
+    } else {
+        Copy-Item $unsignedZipFilename $signedZipFilename
+    }
+}
+
+#
+# 未署名インストーラ($unsignedInstaller) を署名して $signedInstaller へ書き込む
+#
+function SignInstaller {
+    param (
+        [string]$unsignedInstaller,
+        [string]$signedInstaller,
+        [string]$API_TOKEN
+    )
+
+    if ($enableSignInstaller) {
+        Write-Output "Requesting signing for teraterm-${version}.exe"
+        Submit-SigningRequest `
+          -InputArtifactPath "$unsignedInstaller" `
+          -ApiToken $API_TOKEN `
+          -ArtifactConfigurationSlug "installer" `
+          -OrganizationId $ORGANIZATION_ID `
+          -ProjectSlug $PROJECT_SLUG `
+          -SigningPolicySlug $SIGNING_POLICY_SLUG `
+          -OutputArtifactPath "$signedInstaller" `
+          -Force `
+          -WaitForCompletion
+    } else {
+        Copy-Item "$unsignedInstaller" "$signedInstaller"
+    }
+}
+
+#
+# 未署名のインストーラ作成
+#
+function CreateUnsignedInstaller
+{
+    param (
+        [string]$baseIssFilename,
+        [string]$version,
+        [string]$WorkDir
+    )
+
+    Push-Location $WorkDir
+    $issFilename = "teraterm.iss"
+    (Get-Content $baseIssFilename) `
+      -replace 'LicenseFile=release\\license.txt', "LicenseFile=teraterm\license.txt" `
+      -replace 'Source: \.\.\\teraterm\\release', "Source: teraterm" `
+      -replace 'Source: release', "Source: teraterm" `
+      -replace 'Source: \.\.\\doc\\en', "Source: teraterm" `
+      -replace 'Source: \.\.\\doc\\ja', "Source: teraterm" `
+      -replace 'Source: \.\.\\ttssh2\\ttxssh\\Release', "Source: teraterm" `
+      -replace 'Source: \.\.\\cygwin\\cygterm\\cygterm\+-x86_64', "Source: teraterm" `
+      -replace 'Source: \.\.\\cygwin\\cygterm', "Source: teraterm" `
+      -replace 'Source: \.\.\\cygwin\\Release', "Source: teraterm" `
+      -replace 'Source: \.\.\\ttpmenu\\Release', "Source: teraterm" `
+      -replace 'Source: \.\.\\ttpmenu\\readme.txt', "Source: teraterm\ttmenu_readme-j.txt" `
+      -replace 'Source: \.\.\\TTProxy\\Release', "Source: teraterm" `
+      -replace 'Source: \.\.\\TTXKanjiMenu\\release', "Source: teraterm" `
+      -replace 'Source: \.\.\\TTXSamples\\release', "Source: teraterm" `
+      | Set-Content $issFilename
+
+    & "$ISCC" "$issFilename" /DAppVer=${version} /O..
+    remove-Item -Path $issFilename -Force
+    Pop-Location
+}
+
+if (-not (Test-Path $zipFilename)) {
+    Write-Error "Tera Term portable zip file not found: $zipFilename"
+    exit 1
+}
+
+if (Test-Path "teraterm_unsigned.zip") {
+    Remove-Item "teraterm_unsigned.zip"
+}
+if (Test-Path "teraterm-${version}_signed.zip") {
+    Remove-Item "teraterm-${version}_signed.zip"
+}
+
+# SignPath moduleをインストール
+if (-not (Get-Module -ListAvailable -Name SignPath)) {
+    Install-Module -Name SignPath -Force -Scope CurrentUser
+}
+
+# 作業用フォルダ
+$WorkDir = "sign_signpath_{0}" -f ([System.Guid]::NewGuid().ToString("N"))
+$WorkDirTT = Join-Path $WorkDir "teraterm"
+
+# teraterm_unsigned.zip を作る
+#  作ったzip内のフォルダは "teraterm/" になっている
+ExtractTeraTermPortableZip -zipFilename $zipFilename -destination $WorkDirTT
+try {
+    Compress-Archive -Path $WorkDirTT -DestinationPath "teraterm_unsigned.zip"
+} catch {
+    Write-Error """teraterm_unsigned.zip"" error: $_"
+    exit 1
+}
+Remove-Item -Path $WorkDirTT -Recurse -Force
+
+# teraterm_unsigned.zip を署名 teraterm_signed.zip を作る
+SignZip `
+  -unsignedZipFilename "teraterm_unsigned.zip" `
+  -signedZipFilename "teraterm_signed.zip" `
+  -API_TOKEN $API_TOKEN
+remove-Item -Path "teraterm_unsigned.zip" -Force
+
+# 署名済みのポータブルzipを "teraterm/" へ展開
+ExtractTeraTermPortableZip -zipFilename "teraterm_signed.zip" -destination $WorkDirTT
+remove-Item -Path "teraterm_signed.zip" -Force
+
+# フォルダ名を変更して teraterm-${version}_signed.zip を作成
+$WorkDirTTV = Join-Path $WorkDir "teraterm-${version}"
+Copy-Item -Path $WorkDirTT -Destination $WorkDirTTV -Recurse -Force
+Compress-Archive -Path $WorkDirTTV -DestinationPath "teraterm-${version}_signed.zip"
+Remove-Item -Path $WorkDirTTV -Recurse -Force
+
+# 未署名インストーラ作成
+#  (インストーラー本体が未署名、内部にアーカイブされたファイルは署名済み)
+CreateUnsignedInstaller `
+  -baseIssFilename "$SOURCE_DIR/installer/teraterm.iss" `
+  -version ${version}_unsigned `
+  -WorkDir $WorkDir
+Remove-Item -Path $WorkDir -Recurse -Force
+
+# インストーラを署名する
+SignInstaller `
+  -unsignedInstaller "teraterm-${version}_unsigned.exe" `
+  -signedInstaller "teraterm-${version}_signed.exe" `
+  -API_TOKEN $API_TOKEN
+Remove-Item -Path "teraterm-${version}_unsigned.exe" -Force
