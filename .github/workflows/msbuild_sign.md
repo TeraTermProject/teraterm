cb56f02d09a650a0b2aa6ce0a2e8608d47e1198b
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2025-06-15 01:28:02 +0900

    GitHub Actions で SignPath でコード署名できるようにした
    - https://about.signpath.io/
    - SignPath のトークンが設定されているとき署名を行う
      - `secrets.SIGNPATH_API_TOKEN` に設定する
    - 署名済み実行ファイルを含むポータブルzip
    - 署名済み実行ファイルを含む、署名済みインストーラー

---
+++ .github/workflows/msbuild_sign.md
@@ -0,0 +1,75 @@
+﻿
+GitHub Actionsでビルド時の
+バイナリを作成後
+SignPath.io でコード署名の動作
+
+
+```
+
+GitHub                                     SignPath
+
+----------------------------------------+-------------------------------------
+
+- folder
+  - installer/Output/portable
+- contents
+  - portable files (not ziped)
+
+          |
+          V
+
+- actions/upload-artifact@v4
+  - folder
+    - installer/Output/portable
+  - zip
+    - portable_unsigned.zip
+                                       -->
+                                              - Artifact Configurations
+                                                - teraterm_portable
+                                       <--
+- foler
+  - installer/Output/portable_signed
+- contents
+  - portable signd files (not ziped)
+
+              |
+              V
+
+- actions/upload-artifact@v4
+  - folder
+    - installer/Output/portable_signed
+  - contents
+    - portable signd files (not ziped)
+  - zip
+    - portable_zip_signed.zip
+
+              |
+              V
+
+- installer/iscc_signed.cmake
+  - 未署名インストーラー作成
+  - input folder
+    - installer/Output/portable_signed
+  - output folder / filename
+    - installer/Output/setup/teraterm-unsigned.exe
+
+              |
+              V
+
+- actions/upload-artifact@v4
+  - folder
+    - installer/Output/setup/
+  - contents
+    - teraterm-unsigned.exe (unsigned installer)
+  - zip
+    - setup_unsigned
+                                       -->
+                                              - Artifact Configurations
+                                                - ziped_installer [XXX zipped]
+                                       <--
+- foler
+  - installer/Output/portable_signed
+- contents
+  - teraterm-unsigned.exe (signed installer)
+
+```
--- installer/iscc.bat
+++ installer/iscc.bat
@@ -6,40 +6,59 @@ setlocal
 cd /d %~dp0
 
 if not exist Output mkdir Output
-set TT_VERSION=%VERSION%
+if not exist Output\portable mkdir Output\portable
 SET VCSVERSION=%GITVERSION%
 IF NOT "%SVNVERSION%" == "unknown" (
     SET VCSVERSION=r%SVNVERSION%
 )
-set SNAPSHOT_PORTABLE_OUTPUT="teraterm-%TT_VERSION%-%DATE%_%TIME%-%VCSVERSION%-%USERNAME%-snapshot"
-if "%RELEASE%" == "1" (
-    pushd Output
-    %CMAKE% -E tar cf teraterm-%TT_VERSION%.zip --format=zip teraterm-%TT_VERSION%/
-    %CMAKE% -E tar cf teraterm-%TT_VERSION%_pdb.zip --format=zip teraterm-%TT_VERSION%_pdb/
-    popd
-    set INNO_SETUP_OPT_VERSION=
-    set INNO_SETUP_OPT_OUTPUT=
-) else (
-    %CMAKE% -E rename snapshot-%DATE%_%TIME% %SNAPSHOT_PORTABLE_OUTPUT%
-    %CMAKE% -E rename snapshot-%DATE%_%TIME%_pdb %SNAPSHOT_PORTABLE_OUTPUT%_pdb
-    %CMAKE% -E tar cf Output/%SNAPSHOT_PORTABLE_OUTPUT%.zip --format=zip %SNAPSHOT_PORTABLE_OUTPUT%
-    %CMAKE% -E tar cf Output/%SNAPSHOT_PORTABLE_OUTPUT%_pdb.zip --format=zip %SNAPSHOT_PORTABLE_OUTPUT%_pdb
-    %CMAKE% -E rename %SNAPSHOT_PORTABLE_OUTPUT% snapshot-%DATE%_%TIME%
-    %CMAKE% -E rename %SNAPSHOT_PORTABLE_OUTPUT%_pdb snapshot-%DATE%_%TIME%_pdb
-    set INNO_SETUP_OPT_VERSION="/DVerSubStr=%DATE%_%TIME%"-%VCSVERSION%
-    set INNO_SETUP_OPT_OUTPUT="/DOutputSubStr=%DATE%_%TIME%-%VCSVERSION%-%USERNAME%-snapshot"
-)
-%INNO_SETUP% %INNO_SETUP_OPT_VERSION% /DAppVer=%VERSION% %INNO_SETUP_OPT_OUTPUT% teraterm.iss
 
+set SNAPSHOT_OUTPUT="teraterm-%VERSION%-%DATE%_%TIME%-%VCSVERSION%-%USERNAME%-snapshot"
+set RELEASE_OUTPUT="teraterm-%VERSION%"
+
+
+rem ポータブル版をコピーして取っておく(署名に使用する)
+%CMAKE% -E rm -rf Output/portable/teraterm
+%CMAKE% -E copy_directory teraterm Output/portable/teraterm
+
+
+rem (署名なし)インストーラ作成
+if "%RELEASE%" == "1" (
+    set INNO_SETUP_OUTPUT="/DOutputBaseFilename=%RELEASE_OUTPUT%"
+    set INNO_SETUP_APPVERSION="/DAppVersion=%VERSION%"
+) else (
+    set INNO_SETUP_OUTPUT="/DOutputBaseFilename=%SNAPSHOT_OUTPUT%"
+    set INNO_SETUP_APPVERSION="/DAppVersion=%VERSION% %DATE%_%TIME%-%VCSVERSION%"
+)
+%INNO_SETUP% %INNO_SETUP_APPVERSION% /OOutput %INNO_SETUP_OUTPUT% /DSrcDir=teraterm teraterm.iss
+
+
+rem (署名なし)ポータブル版のzipを作成
+if "%RELEASE%" == "1" (
+    %CMAKE% -E rm -rf %RELEASE_OUTPUT%
+    %CMAKE% -E rm -rf %RELEASE_OUTPUT%_pdb
+    %CMAKE% -E copy_directory teraterm %RELEASE_OUTPUT%
+    %CMAKE% -E copy_directory teraterm_pdb %RELEASE_OUTPUT%_pdb
+    %CMAKE% -E tar cf Output/%RELEASE_OUTPUT%.zip --format=zip %RELEASE_OUTPUT%/
+    %CMAKE% -E tar cf Output/%RELEASE_OUTPUT%_pdb.zip --format=zip %RELEASE_OUTPUT%_pdb/
+) else (
+    %CMAKE% -E rm -rf %SNAPSHOT_OUTPUT%
+    %CMAKE% -E rm -rf %SNAPSHOT_OUTPUT%_pdb
+    %CMAKE% -E copy_directory teraterm %SNAPSHOT_OUTPUT%
+    %CMAKE% -E copy_directory teraterm_pdb %SNAPSHOT_OUTPUT%_pdb
+    %CMAKE% -E tar cf Output/%SNAPSHOT_OUTPUT%.zip --format=zip %SNAPSHOT_OUTPUT%
+    %CMAKE% -E tar cf Output/%SNAPSHOT_OUTPUT%_pdb.zip --format=zip %SNAPSHOT_OUTPUT%_pdb
+)
+
+rem hash
 if "%RELEASE%" == "1" (
     pushd Output
-    %CMAKE% -DOUTPUT=teraterm-%TT_VERSION%.sha256sum -P ../sha256sum.cmake teraterm-%TT_VERSION%.exe teraterm-%TT_VERSION%.zip teraterm-%TT_VERSION%_pdb.zip
-    %CMAKE% -DOUTPUT=teraterm-%TT_VERSION%.sha512sum -P ../sha512sum.cmake teraterm-%TT_VERSION%.exe teraterm-%TT_VERSION%.zip teraterm-%TT_VERSION%_pdb.zip
+    %CMAKE% -DOUTPUT=%RELEASE_OUTPUT%.sha256sum -P ../sha256sum.cmake %RELEASE_OUTPUT%.exe %RELEASE_OUTPUT%.zip %RELEASE_OUTPUT%_pdb.zip
+    %CMAKE% -DOUTPUT=%RELEASE_OUTPUT%.sha512sum -P ../sha512sum.cmake %RELEASE_OUTPUT%.exe %RELEASE_OUTPUT%.zip %RELEASE_OUTPUT%_pdb.zip
     popd
 ) else (
     pushd Output
-    %CMAKE% -DOUTPUT=%SNAPSHOT_PORTABLE_OUTPUT%.sha256sum -P ../sha256sum.cmake %SNAPSHOT_PORTABLE_OUTPUT%.exe %SNAPSHOT_PORTABLE_OUTPUT%.zip %SNAPSHOT_PORTABLE_OUTPUT%_pdb.zip
-    %CMAKE% -DOUTPUT=%SNAPSHOT_PORTABLE_OUTPUT%.sha512sum -P ../sha512sum.cmake %SNAPSHOT_PORTABLE_OUTPUT%.exe %SNAPSHOT_PORTABLE_OUTPUT%.zip %SNAPSHOT_PORTABLE_OUTPUT%_pdb.zip
+    %CMAKE% -DOUTPUT=%SNAPSHOT_OUTPUT%.sha256sum -P ../sha256sum.cmake %SNAPSHOT_OUTPUT%.exe %SNAPSHOT_OUTPUT%.zip %SNAPSHOT_OUTPUT%_pdb.zip
+    %CMAKE% -DOUTPUT=%SNAPSHOT_OUTPUT%.sha512sum -P ../sha512sum.cmake %SNAPSHOT_OUTPUT%.exe %SNAPSHOT_OUTPUT%.zip %SNAPSHOT_OUTPUT%_pdb.zip
     popd
 )
 
