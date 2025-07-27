180b8ee3191841a3435a648fbd121919df8fa295
nmaya <5668936+nmaya@users.noreply.github.com>
2025-07-27 10:54:32 +0900

    zlib ビルドの出力先を変更 #228
    - ビルドを win32\Makefile.msc から CMake に変更
    - CMake を使うと zconf.h が消えるので、include にビルドディレクトリを追加

--- libs/buildzlib.bat
+++ libs/buildzlib.bat
@@ -1,29 +1,71 @@
-cd zlib
+rem Build zlib
 
-if not exist "win32\Makefile.msc.release" goto mkmf
-for %%F in (win32\Makefile.msc.release) do set mftime=%%~tF
-for %%F in (..\buildzlib.bat) do set battime=%%~tF
-if "%battime%" leq "%mftime%" goto build
+pushd zlib
 
-del zlibd.lib
-nmake -f win32\Makefile.msc.release clean
 
-:mkmf
-perl -e "open(IN,'win32\Makefile.msc');while(<IN>){s/ -MD/ -MT/;print $_;}close(IN);" > win32\Makefile.msc.release
+rem Find Visual Studio
+if not "%VSINSTALLDIR%" == "" goto vsinstdir
 
-perl -e "open(IN,'win32\Makefile.msc');while(<IN>){s/ -MD/ -MTd/;s/ -O2/ -Od/;s/ -release/ -debug/;s/ zlib.lib/ zlibd.lib/;s/ zlib.lib/ zlibd.lib/;print $_;}close(IN);" > win32\Makefile.msc.debug
+:check_2019
+if "%VS160COMNTOOLS%" == "" goto check_2022
+if not exist "%VS160COMNTOOLS%\VsDevCmd.bat" goto check_2022
+call "%VS160COMNTOOLS%\VsDevCmd.bat"
+goto vs2019
 
-:build
-if exist zlibd.lib goto build_release
-nmake -f win32\Makefile.msc.debug clean
-nmake -f win32\Makefile.msc.debug OBJA="inffast.obj"
-move zlibd.lib examples\
-move zlib.pdb examples\
-nmake -f win32\Makefile.msc.release clean
-move examples\zlibd.lib .\
-move examples\zlib.pdb .\
+:check_2022
+if "%VS170COMNTOOLS%" == "" goto novs
+if not exist "%VS170COMNTOOLS%\VsDevCmd.bat" goto novs
+call "%VS170COMNTOOLS%\VsDevCmd.bat"
+goto vs2022
 
-:build_release
-nmake -f win32\Makefile.msc.release OBJA="inffast.obj"
+:novs
+echo "Can't find Visual Studio"
+goto fail
 
-cd ..
+:vsinstdir
+rem Check Visual Studio version
+set VSCMNDIR="%VSINSTALLDIR%\Common7\Tools\"
+set VSCMNDIR=%VSCMNDIR:\\=\%
+
+if /I %VSCMNDIR% EQU "%VS160COMNTOOLS%" goto vs2019
+if /I %VSCMNDIR% EQU "%VS170COMNTOOLS%" goto vs2022
+
+echo Unknown Visual Studio version
+goto fail
+
+
+rem Generate Makefile
+rem -DCMAKE_MSVC_RUNTIME_LIBRARY="MultiThreaded" を指定すると、Debug が MultiThreadedDebug にならず MultiThreaded になるので指定しない
+:vs2019
+cmake -G "Visual Studio 16 2019" -A Win32 -S . -B build\Win32
+goto gen_end
+
+:vs2022
+cmake -G "Visual Studio 17 2022" -A Win32 -S . -B build\Win32
+goto gen_end
+
+:gen_end
+
+rem libz must be /MT(d)
+perl -e "open(IN,'build\Win32\zlibstatic.vcxproj');while(<IN>){s/MultiThreadedDebugDLL/MultiThreadedDebug/;print $_;}close(IN);" > build\Win32\zlibstatic.vcxproj.tmp
+perl -e "open(IN,'build\Win32\zlibstatic.vcxproj.tmp');while(<IN>){s/MultiThreadedDLL/MultiThreaded/;print $_;}close(IN);" > build\Win32\zlibstatic.vcxproj.tmp2
+COPY build\Win32\zlibstatic.vcxproj.tmp2 build\Win32\zlibstatic.vcxproj
+DEL build\Win32\zlibstatic.vcxproj.tmp
+DEL build\Win32\zlibstatic.vcxproj.tmp2
+
+
+rem Build
+cmake --build build\Win32 --target zlibstatic --config Debug
+cmake --build build\Win32 --target zlibstatic --config Release
+
+
+:end
+popd
+exit /b 0
+
+
+:fail
+popd
+echo "buildzlib.bat failed"
+@echo on
+exit /b 1
