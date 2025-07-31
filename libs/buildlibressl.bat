92a5e866e4c6c13ce7772ba77352e135443a3fed
nmaya <5668936+nmaya@users.noreply.github.com>
2025-08-01 07:38:46 +0900

    ビルドの Target Platform に変数を使うようにした #228

--- libs/buildlibressl.bat
+++ libs/buildlibressl.bat
@@ -1,6 +1,15 @@
 rem Build LibreSSL
+setlocal
 
 pushd libressl
+
+
+rem architecture for VsDevCmd.bat
+if "%TARGET%" == "" (set TARGET=Win32)
+if "%TARGET%" == "Win32" (set ARCHITECTURE=x86)
+if "%TARGET%" == "x64"   (set ARCHITECTURE=x64)
+if "%TARGET%" == "ARM64" (set ARCHITECTURE=arm64)
+if "%TARGET%" == "ARM64" if "%HOST_ARCHITECTURE%" == "" (set HOST_ARCHITECTURE=amd64)
 
 
 rem Find Visual Studio
@@ -9,13 +18,13 @@ if not "%VSINSTALLDIR%" == "" goto vsinstdir
 :check_2019
 if "%VS160COMNTOOLS%" == "" goto check_2022
 if not exist "%VS160COMNTOOLS%\VsDevCmd.bat" goto check_2022
-call "%VS160COMNTOOLS%\VsDevCmd.bat"
+call "%VS160COMNTOOLS%\VsDevCmd.bat" -arch=%ARCHITECTURE% -host_arch=%HOST_ARCHITECTURE%
 goto vs2019
 
 :check_2022
 if "%VS170COMNTOOLS%" == "" goto novs
 if not exist "%VS170COMNTOOLS%\VsDevCmd.bat" goto novs
-call "%VS170COMNTOOLS%\VsDevCmd.bat"
+call "%VS170COMNTOOLS%\VsDevCmd.bat" -arch=%ARCHITECTURE% -host_arch=%HOST_ARCHITECTURE%
 goto vs2022
 
 :novs
@@ -36,22 +45,23 @@ goto fail
 
 rem Generate Makefile
 :vs2019
-set CMAKE_PARAMETER=-G "Visual Studio 16 2019" -A Win32 -B build\Win32
+set CMAKE_PARAMETER=-G "Visual Studio 16 2019" -A %TARGET% -B build\%TARGET%
 goto gen_end
 
 :vs2022
-set CMAKE_PARAMETER=-G "Visual Studio 17 2022" -A Win32 -B build\Win32
+set CMAKE_PARAMETER=-G "Visual Studio 17 2022" -A %TARGET% -B build\%TARGET%
 goto gen_end
 
 :gen_end
 
 cmake -DMSVC=on -DUSE_STATIC_MSVC_RUNTIMES=on %CMAKE_PARAMETER%
-cmake --build build\Win32 --target crypto --config Debug
-cmake --build build\Win32 --target crypto --config Release
+cmake --build build\%TARGET% --target crypto --config Debug
+cmake --build build\%TARGET% --target crypto --config Release
 
 
 :end
 popd
+endlocal
 exit /b 0
 
 
@@ -59,4 +69,5 @@ exit /b 0
 popd
 echo "buildlibressl.bat failed"
 @echo on
+endlocal
 exit /b 1
