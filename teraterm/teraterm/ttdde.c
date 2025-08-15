0894285419317be8e7734757b8863389f1453280
hkanou <void888888@gmail.com>
2025-08-15 22:32:08 +0900

    setserialdelaychar, setserialdelayline マクロコマンドを修正 #437 - 送信遅延ミリ秒に0を指定出来るよう修正した - 送信遅延の変更が起動中のマクロに反映されるよう修正した (ComVar構造体:cvに設定) - 送信バッファが空になってから送信遅延が反映されるように修正した (GetOutBuffInfoで送信バッファが空か確認) - 送信遅延の変更が終わってから次のマクロコマンドを実行するよう変更した (WM_USER_DDECMNDENDを待つよう修正) - 送信バッファ(sendmem_fifo)の数を10から無制限に変更した

--- teraterm/teraterm/ttdde.c
+++ teraterm/teraterm/ttdde.c
@@ -73,6 +73,7 @@ char TopicName[21] = "";
 static HCONV ConvH = 0;
 BOOL AdvFlag = FALSE;
 BOOL CloseTT = FALSE;
+BOOL isSetSerialDelay = FALSE;	// for setserialdelaychar, setserialdelayline
 
 static BOOL DdeCmnd = FALSE;
 
@@ -1054,8 +1055,18 @@ static HDDEDATA AcceptExecute(HSZ TopicHSz, HDDEDATA Data)
 			}
 
 			val = atoi(ParamFileName);
-			if (val > 0) {
+			if (val >= 0) {
 				ts.DelayPerChar = val;
+				size_t use;
+				GetOutBuffInfo(&cv, &use, NULL);
+				if (use == 0) {
+					cv.DelayPerChar = val;
+					if (HWndDdeCli != NULL) {
+						PostMessage(HWndDdeCli,WM_USER_DDECMNDEND,1,0);
+					}
+				} else {
+					isSetSerialDelay = TRUE;
+				}
 			}
 		}
 		break;
@@ -1070,8 +1081,18 @@ static HDDEDATA AcceptExecute(HSZ TopicHSz, HDDEDATA Data)
 			}
 
 			val = atoi(ParamFileName);
-			if (val > 0) {
+			if (val >= 0) {
 				ts.DelayPerLine = val;
+				size_t use;
+				GetOutBuffInfo(&cv, &use, NULL);
+				if (use == 0) {
+					cv.DelayPerLine = val;
+					if (HWndDdeCli != NULL) {
+						PostMessage(HWndDdeCli,WM_USER_DDECMNDEND,1,0);
+					}
+				} else {
+					isSetSerialDelay = TRUE;
+				}
 			}
 		}
 		break;
@@ -1517,4 +1538,14 @@ void RunMacro(PCHAR FName, BOOL Startup)
 	wchar_t *fnameW = ToWcharA(FName);
 	RunMacroW(fnameW, Startup);
 	free(fnameW);
+}
+
+void SetSerialDelayEnd(int Result)
+{
+	isSetSerialDelay = FALSE;
+	cv.DelayPerChar = ts.DelayPerChar;
+	cv.DelayPerLine = ts.DelayPerLine;
+	if (HWndDdeCli != NULL) {
+		PostMessage(HWndDdeCli,WM_USER_DDECMNDEND,(WPARAM)Result,0);
+	}
 }
