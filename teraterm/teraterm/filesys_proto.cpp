0d30f7b02c2a0754b25f1e6404d663c5ab81145f
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2025-12-07 23:32:36 +0900

    OPENFILENAMEW.lStructSize に設定する値を修正 #1001
    - 修正前はANSI版のサイズを設定していた
    
    (cherry picked from commit 5096843990762755f9f21fee68ebeb0479896f53)

--- teraterm/teraterm/filesys_proto.cpp
+++ teraterm/teraterm/filesys_proto.cpp
@@ -728,7 +728,7 @@ static wchar_t **_GetXFname(HWND HWin, BOOL Receive, const wchar_t *caption, LPL
 	}
 
 	OPENFILENAMEW ofn = {};
-	ofn.lStructSize = get_OPENFILENAME_SIZE();
+	ofn.lStructSize = get_OPENFILENAME_SIZEW();
 	ofn.hwndOwner   = HWin;
 	ofn.lpstrFilter = FNFilter;
 	ofn.nFilterIndex = 1;
@@ -998,7 +998,7 @@ static wchar_t **_GetMultiFname(HWND hWnd, WORD FuncId, const wchar_t *caption, 
 	}
 
 	OPENFILENAMEW ofn = {};
-	ofn.lStructSize = get_OPENFILENAME_SIZE();
+	ofn.lStructSize = get_OPENFILENAME_SIZEW();
 	ofn.hwndOwner   = hWnd;
 	ofn.lpstrFilter = FNFilter;
 	ofn.nFilterIndex = 1;
@@ -1598,7 +1598,7 @@ static wchar_t **_GetTransFname(HWND hWnd, const wchar_t *DlgCaption)
 
 	FileName[0] = 0;
 	OPENFILENAMEW ofn = {};
-	ofn.lStructSize = get_OPENFILENAME_SIZE();
+	ofn.lStructSize = get_OPENFILENAME_SIZEW();
 	ofn.hwndOwner   = hWnd;
 	ofn.lpstrFilter = FNFilter;
 	ofn.nFilterIndex = 1;
