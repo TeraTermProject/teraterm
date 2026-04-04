1470808eda598584ddb3311a93df79a177d4cda7
hkanou <void888888@gmail.com>
2026-04-04 23:24:55 +0900

    ttermpro.exe の /M オプションを使用した際、connect コマンドで開いた VT Window が最小化状態で起動し、マクロコマンドが実行されない不具合を修正した #1195

--- teraterm/ttpmacro/ttl.cpp
+++ teraterm/ttpmacro/ttl.cpp
@@ -634,7 +634,7 @@ static WORD TTLConnect(WORD mode)
 
 		strW = ToWcharU8(Str);
 		aswprintf(&command_line, L"%hs /D=%hs %s", command, TopicNameA, strW);
-		e = TTWinExec(command_line);
+		e = TTWinExecWithShowWindow(command_line, SW_NORMAL);
 		free(command_line);
 		free(strW);
 		if (e != NO_ERROR) {
