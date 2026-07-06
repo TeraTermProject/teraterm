077f12b97045ef6a722a92f1091da28adf20d9a4
hkanou <void888888@gmail.com>
2026-07-06 22:33:39 +0900

    XMODEMの進捗ダイアログの更新頻度を10回/秒に変更した #1408
    (cherry picked from commit 7f6355e7fd56ba84a8e79d1530da5eb1a0857f33)

--- teraterm/ttpfile/xmodem.c
+++ teraterm/ttpfile/xmodem.c
@@ -77,6 +77,7 @@ typedef struct {
 	BOOL FileOpen;
 	LONG FileSize;
 	LONG ByteCount;
+ 	DWORD PrevElapsed;
 
 	int ProgStat;
 
@@ -97,6 +98,32 @@ typedef struct {
 	TFileIO *file;
 } TXVar;
 typedef TXVar *PXVar;
+
+static void update_dialog(PXVar xv, BOOL force_update)
+{
+	PFileVarProto fv = xv->fv;
+ 	DWORD elapsed;
+
+	elapsed = (GetTickCount() - xv->StartTime) / 100; // 更新頻度は10回/秒
+	if (elapsed != xv->PrevElapsed || force_update) {
+		if (xv->XMode == IdXReceive) {
+			xv->PrevElapsed = elapsed;
+			fv->InfoOp->SetDlgPacketNum(fv, xv->PktNumOffset + xv->PktNum);
+			fv->InfoOp->SetDlgByteCount(fv, xv->ByteCount);
+			fv->InfoOp->SetDlgTime(fv, xv->StartTime, xv->ByteCount);
+		} else if (xv->XMode == IdXSend) {
+			xv->PrevElapsed = elapsed;
+			if (xv->PktNumSent == 0) {
+				fv->InfoOp->SetDlgPacketNum(fv, xv->PktNumOffset + 256);
+			} else {
+				fv->InfoOp->SetDlgPacketNum(fv, xv->PktNumOffset + xv->PktNumSent);
+			}
+			fv->InfoOp->SetDlgByteCount(fv, xv->ByteCount);
+			fv->InfoOp->SetDlgPercent(fv, xv->ByteCount, xv->FileSize, &xv->ProgStat);
+			fv->InfoOp->SetDlgTime(fv, xv->StartTime, xv->ByteCount);
+		}
+	}
+}
 
 static int XRead1Byte(PXVar xv, LPBYTE b)
 {
@@ -301,6 +328,7 @@ static BOOL XInit(TProto *pv, PComVar cv, PTTSet ts)
 	}
 	fv->InfoOp->SetDlgProtoFileName(fv, xv->FullName);
 	xv->StartTime = 0;
+	xv->PrevElapsed = 0;
 
 	xv->PktNumOffset = 0;
 	xv->PktNum = 0;
@@ -366,6 +394,7 @@ static void XCancel(TProto *pv)
 	} else {
 		XCancel_(xv);
 	}
+	update_dialog(xv, TRUE);
 }
 
 static void XTimeOutProc(TProto *pv)
@@ -379,6 +408,7 @@ static void XTimeOutProc(TProto *pv)
 		XSendNAK(xv);
 		break;
 	}
+	update_dialog(xv, TRUE);
 }
 
 static BOOL XReadPacket(PXVar xv)
@@ -415,6 +445,7 @@ static BOOL XReadPacket(PXVar xv)
 				b = ACK;
 				fv->Success = TRUE;
 				XWrite(xv, &b, 1);
+				update_dialog(xv, TRUE);
 				return FALSE;
 				break;
 			case CAN:
@@ -423,6 +454,7 @@ static BOOL XReadPacket(PXVar xv)
 					continue;
 				}
 				else {
+					update_dialog(xv, TRUE);
 					return FALSE;
 				}
 				break;
@@ -536,11 +568,7 @@ static BOOL XReadPacket(PXVar xv)
 	}
 
 	xv->ByteCount = xv->ByteCount + c;
-
-	fv->InfoOp->SetDlgPacketNum(fv, xv->PktNumOffset + xv->PktNum);
-	fv->InfoOp->SetDlgByteCount(fv, xv->ByteCount);
-	fv->InfoOp->SetDlgTime(fv, xv->StartTime, xv->ByteCount);
-
+	update_dialog(xv, FALSE);
 	fv->FTSetTimeOut(fv, xv->TOutLong);
 
 	return TRUE;
@@ -566,6 +594,7 @@ static BOOL XSendPacket(PXVar xv)
 			case ACK:
 				if (!xv->FileOpen) {
 					fv->Success = TRUE;
+					update_dialog(xv, TRUE);
 					return FALSE;
 				} else if (xv->PktNumSent == (BYTE) (xv->PktNum + 1)) {
 					xv->PktNum = xv->PktNumSent;
@@ -595,6 +624,7 @@ static BOOL XSendPacket(PXVar xv)
 					continue;
 				}
 				else {
+					update_dialog(xv, TRUE);
 					return FALSE;
 				}
 				break;
@@ -617,6 +647,7 @@ static BOOL XSendPacket(PXVar xv)
 
 		if (xv->isXCancel) {
 			XCancel_(xv); // ブロック境界(ACK/NAKの直後)で CAN を送信する
+			update_dialog(xv, TRUE);
 			return FALSE;
 		}
 
@@ -697,15 +728,7 @@ static BOOL XSendPacket(PXVar xv)
 	}
 
 	if (xv->PktBufCount == 0) {
-		if (xv->PktNumSent == 0) {
-			fv->InfoOp->SetDlgPacketNum(fv, xv->PktNumOffset + 256);
-		}
-		else {
-			fv->InfoOp->SetDlgPacketNum(fv, xv->PktNumOffset + xv->PktNumSent);
-		}
-		fv->InfoOp->SetDlgByteCount(fv, xv->ByteCount);
-		fv->InfoOp->SetDlgPercent(fv, xv->ByteCount, xv->FileSize, &xv->ProgStat);
-		fv->InfoOp->SetDlgTime(fv, xv->StartTime, xv->ByteCount);
+		update_dialog(xv, !xv->FileOpen);
 	}
 
 	return TRUE;
