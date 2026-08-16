36b7c03ff8995a82f180efa1444a55d731486e78
zmatsuo <6488847+zmatsuo@users.noreply.github.com>
2026-08-16 11:52:31 +0900

    PATH 上の perl で HTML::Parser が使えないときは buildtools/ の perl を使用する #1481

--- doc/linkchecker.bat
+++ doc/linkchecker.bat
@@ -6,6 +6,11 @@ cd /d %CUR%
 set NOPAUSE=1
 call ..\buildtools\install_linkchecker.bat
 call ..\buildtools\find_perl.bat
+rem PATH ã‚Ì perl ‚Å HTML::Parser ‚ªŽg‚¦‚È‚¢‚Æ‚«‚Í buildtools ‚Ì perl ‚ðŽg—p‚·‚é
+%PERL% -MHTML::Parser -e 1 > nul 2>&1
+if %errorlevel% == 0 goto perl_found
+set PERL=%CUR%..\buildtools\cygwin64\bin\perl.exe
+:perl_found
 
 pushd ja\html
 echo ja
